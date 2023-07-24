e310e35b0f43a29fcd48a3d436042cd4606e6639
Grace Choi <grace.54109@gmail.com>
2023-07-24 12:06:05 -0500

    Deleted old code

--- src/datasources/system/SystemDataSource.ts
+++ src/datasources/system/SystemDataSource.ts
@@ -18,25 +18,6 @@ export class SystemDataSource extends DataSourceApi<SystemQuery> {
     this.baseUrl = this.instanceSettings.url + '/nisysmgmt/v1';
   }
 
-  // async query(options: DataQueryRequest<SystemQuery>): Promise<DataQueryResponse> {
-  //   const { range } = options;
-  //   const from = range!.from.valueOf();
-  //   const to = range!.to.valueOf();
-
-  //   // Return a constant for each query.
-  //   const data = options.targets.map((target) => {
-  //     return new MutableDataFrame({
-  //       refId: target.refId,
-  //       fields: [
-  //         { name: 'Time', values: [from, to], type: FieldType.time },
-  //         { name: 'Value', values: [1, 2], type: FieldType.number },
-  //       ],
-  //     });
-  //   });
-
-  //   return { data };
-  // }
-
   async query(options: DataQueryRequest<SystemQuery>): Promise<DataQueryResponse> {
     const { range } = options;
     const from = range!.from.valueOf();
