0cd4a4c74eef09a45b2abf97e6878c633a016d96
Carson Moore <carson.moore@ni.com>
2023-07-28 22:56:02 -0500

    add tag path field + more test fixtures

--- src/datasources/system/module.ts
+++ src/datasources/system/module.ts
@@ -1,7 +1,7 @@
 import { DataSourcePlugin } from '@grafana/data';
 import { SystemDataSource } from './SystemDataSource';
 import { SystemQueryEditor } from './components/SystemQueryEditor';
-import { HttpConfigEditor } from 'core/HttpConfigEditor';
+import { HttpConfigEditor } from 'core/components/HttpConfigEditor';
 
 export const plugin = new DataSourcePlugin(SystemDataSource)
   .setConfigEditor(HttpConfigEditor)
--- src/datasources/tag/components/TagQueryEditor.tsx
+++ src/datasources/tag/components/TagQueryEditor.tsx
@@ -1,32 +1,23 @@
-import React, { ChangeEvent } from 'react';
-import { InlineField, Input } from '@grafana/ui';
+import React, { FormEvent } from 'react';
+import { AutoSizeInput } from '@grafana/ui';
 import { QueryEditorProps } from '@grafana/data';
 import { TagDataSource } from '../TagDataSource';
 import { TagQuery } from '../types';
+import { InlineField } from 'core/components/InlineField';
 
 type Props = QueryEditorProps<TagDataSource, TagQuery>;
 
 export function TagQueryEditor({ query, onChange, onRunQuery }: Props) {
-  const onQueryTextChange = (event: ChangeEvent<HTMLInputElement>) => {
-    onChange({ ...query, queryText: event.target.value });
-  };
-
-  const onConstantChange = (event: ChangeEvent<HTMLInputElement>) => {
-    onChange({ ...query, constant: parseFloat(event.target.value) });
-    // executes the query
+  const onPathChange = (event: FormEvent<HTMLInputElement>): void => {
+    onChange({ ...query, path: event.currentTarget.value });
     onRunQuery();
   };
 
-  const { queryText, constant } = query;
-
   return (
-    <div className="gf-form">
-      <InlineField label="Constant">
-        <Input onChange={onConstantChange} value={constant} width={8} type="number" step="0.1" />
+    <>
+      <InlineField label="Tag path">
+        <AutoSizeInput minWidth={20} defaultValue={query.path} onCommitChange={onPathChange} />
       </InlineField>
-      <InlineField label="Query Text" labelWidth={16} tooltip="Not used yet">
-        <Input onChange={onQueryTextChange} value={queryText || ''} />
-      </InlineField>
-    </div>
+    </>
   );
 }
