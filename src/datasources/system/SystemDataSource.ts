869b01ca68e2668c989483b0407bed0fab50062e
ggracechoi <52022191+ggracechoi@users.noreply.github.com>
2023-08-11 15:24:39 -0500

    feat: display workspace alias instead of workspace ID (#13)
    Co-authored-by: Carson Moore <carson.moore@ni.com>
    Co-authored-by: Cameron Waterman <cameron.waterman@ni.com>

--- src/datasources/system/SystemDataSource.ts
+++ src/datasources/system/SystemDataSource.ts
@@ -12,15 +12,17 @@ import {
 
 import { TestingStatus, getBackendSrv, getTemplateSrv } from '@grafana/runtime';
 
-import { QueryType, SystemMetadata, SystemQuery, SystemSummary, VariableQuery } from './types';
+import { AuthResponse, QueryType, SystemMetadata, SystemQuery, SystemSummary, VariableQuery, Workspace } from './types';
 import { defaultProjection } from './constants';
 import { NetworkUtils } from './network-utils';
 
 export class SystemDataSource extends DataSourceApi<SystemQuery> {
   baseUrl: string;
+  authUrl: string;
   constructor(private instanceSettings: DataSourceInstanceSettings) {
     super(instanceSettings);
     this.baseUrl = this.instanceSettings.url + '/nisysmgmt/v1';
+    this.authUrl = this.instanceSettings.url + '/niauth/v1/auth';
   }
 
   transformProjection(projections: string[]): string {
@@ -35,6 +37,16 @@ export class SystemDataSource extends DataSourceApi<SystemQuery> {
     });
 
     return result;
+  }
+
+  getWorkspaceName(workspaces: Workspace[], system: SystemMetadata): string {
+    for (let i = 0; i < workspaces.length; i++) {
+      if (workspaces[i].id === system.workspace) {
+        return workspaces[i].name;
+      }
+    }
+
+    return system.workspace;
   }
 
   private getIpAddress(ip4Interface: Record<string, string[]>, ip6Interface: Record<string, string[]>): string | null {
@@ -67,7 +79,8 @@ export class SystemDataSource extends DataSourceApi<SystemQuery> {
           projection: this.transformProjection(defaultProjection),
           orderBy: 'createdTimeStamp DESC'
         };
-        let metadataResponse = await getBackendSrv().post<{ data: SystemMetadata[] }>(this.baseUrl + '/query-systems', postBody);
+        const metadataResponse = await getBackendSrv().post<{ data: SystemMetadata[] }>(this.baseUrl + '/query-systems', postBody);
+        const aliasNameResponse = await getBackendSrv().get< AuthResponse >(this.authUrl, postBody);
         return toDataFrame({
           fields: [
             { name: 'id', values: metadataResponse.data.map(m => m.id) },
@@ -79,7 +92,7 @@ export class SystemDataSource extends DataSourceApi<SystemQuery> {
             { name: 'vendor', values: metadataResponse.data.map(m => m.vendor) },
             { name: 'operating system', values: metadataResponse.data.map(m => m.osFullName) },
             { name: 'ip address', values: metadataResponse.data.map(m => this.getIpAddress(m.ip4Interfaces, m.ip6Interfaces)) },
-            { name: 'workspace', values: metadataResponse.data.map(m => m.workspace) },
+            { name: 'workspace', values: metadataResponse.data.map(m => this.getWorkspaceName(aliasNameResponse.workspaces, m)) }
           ]
         });
       }
