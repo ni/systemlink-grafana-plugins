ca3927463900081d3a7b483b4a982aa51deb776f
biggestboy26 <131713103+iadamjee@users.noreply.github.com>
2023-08-15 08:44:07 -0400

    added aggs to query editor

--- src/datasources/tag/components/TagQueryEditor.tsx
+++ src/datasources/tag/components/TagQueryEditor.tsx
@@ -1,22 +1,37 @@
 import React, { FormEvent } from 'react';
-import { AutoSizeInput } from '@grafana/ui';
+import { AutoSizeInput, InlineField } from '@grafana/ui';
 import { QueryEditorProps } from '@grafana/data';
 import { TagDataSource } from '../TagDataSource';
 import { TagQuery } from '../types';
-import { InlineField } from 'core/components/InlineField';
 
 type Props = QueryEditorProps<TagDataSource, TagQuery>;
 
 export function TagQueryEditor({ query, onChange, onRunQuery }: Props) {
+  const options = [
+    { label: 'Min', value: 'min' },
+    { label: 'Max', value: 'max' },
+    { label: 'Mean', value: 'mean' },
+    { label: 'Count', value: 'count' },
+  ];
+
   const onPathChange = (event: FormEvent<HTMLInputElement>): void => {
     onChange({ ...query, path: event.currentTarget.value });
+    onRunQuery();
+  };
+
+  const handleAggChange = (val: SelectableValue<string>['']) => {
+    set_agg(val);
+    query.aggregates = val;
     onRunQuery();
   };
 
   return (
     <>
       <InlineField label="Tag path">
-        <AutoSizeInput minWidth={20} defaultValue={query.path} onCommitChange={onPathChange} />
+        <AutoSizeInput minWidth={150} defaultValue={query.path} onCommitChange={onPathChange} />
+      </InlineField>
+      <InlineField label="Aggregates">
+        <MultiSelect options={options} value={current_agg} onChange={handleAggChange} onBlur={onRunQuery} />
       </InlineField>
     </>
   );
