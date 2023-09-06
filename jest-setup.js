3f56d62f7655aa3f55cfa8f4b94669f1388edd67
Carson <carson.moore@ni.com>
2023-09-06 14:45:34 -0500

    feat(tag): add user-friendly workspace dropdown (#25)

--- jest-setup.js
+++ jest-setup.js
@@ -1,7 +1,14 @@
 // Jest setup provided by Grafana scaffolding
 import './.config/jest-setup';
+import { DataSourceBase } from './src/core/DataSourceBase';
 
 // Called by @grafana/ui AutoSizeInput
 HTMLCanvasElement.prototype.getContext = () => ({
-  measureText: (text) => ({ width: text.length * 8 }),
+  measureText: text => ({ width: text.length * 8 }),
 });
+
+// Default workspaces for tests
+DataSourceBase.Workspaces = [
+  { id: '1', name: 'Default workspace' },
+  { id: '2', name: 'Other workspace' },
+];
