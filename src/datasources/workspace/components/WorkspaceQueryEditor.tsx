8542a45d6e288c6d50166b9d46c90f6319e84fe1
Cameron Waterman <cameron.waterman@ni.com>
2024-01-04 08:59:00 -0600

    Run query on load

--- src/datasources/workspace/components/WorkspaceQueryEditor.tsx
+++ src/datasources/workspace/components/WorkspaceQueryEditor.tsx
@@ -5,8 +5,16 @@ import { WorkspaceDataSource } from '../WorkspaceDataSource';
 
 type Props = QueryEditorProps<WorkspaceDataSource, WorkspaceQuery>;
 
-export function WorkspaceQueryEditor({ onRunQuery }: Props) {
-  useEffect(onRunQuery, [onRunQuery]);
+export function WorkspaceQueryEditor({ query, onChange, onRunQuery }: Props) {
+  // useEffect(onRunQuery, [onRunQuery]);
+
+  // query = datasource.prepareQuery(query);
+
+  useEffect(() => {
+    onChange(query);
+    onRunQuery();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);  // Only run on mount
 
   return (
     <span>This data source returns all SystemLink workspaces and does not include a query editor.</span>
