8a13fbfa9d7fd09764d4aadf8ec09356cc01c6d9
Cameron Waterman <cameron.waterman@ni.com>
2024-01-04 09:39:11 -0600

    Fix workspace data source

--- src/datasources/workspace/components/WorkspaceQueryEditor.tsx
+++ src/datasources/workspace/components/WorkspaceQueryEditor.tsx
@@ -6,12 +6,11 @@ import { WorkspaceDataSource } from '../WorkspaceDataSource';
 type Props = QueryEditorProps<WorkspaceDataSource, WorkspaceQuery>;
 
 export function WorkspaceQueryEditor({ query, onChange, onRunQuery }: Props) {
-  // useEffect(onRunQuery, [onRunQuery]);
-
-  // query = datasource.prepareQuery(query);
-
   useEffect(() => {
-    onChange(query);
+    // The "init" property is required for the initial query in Explore to work since the query
+    // only runs if a change to a property is detected. It otherwise has no use and can be removed
+    // if a property is added to "WorkspaceQuery".
+    onChange(Object.assign({ init: true }, query));
     onRunQuery();
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);  // Only run on mount
