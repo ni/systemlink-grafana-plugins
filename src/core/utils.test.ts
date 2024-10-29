0c27d975d41e143e2abf34d1777906c9c062493e
NI\akerezsi <alex.kerezsi@ni.com>
2024-10-29 17:38:04 +0200

    Refactored unit tests

--- src/core/utils.test.ts
+++ src/core/utils.test.ts
@@ -15,27 +15,31 @@ test('enumToOptions', () => {
 });
 
 
-test('filterXSSLINQExpression', () => {
-    const result = filterXSSLINQExpression('test<script>alert("XSS")</script>');
+describe("filterXSSLINQExpression", () => {
+    test('simple XSS handling', () => {
+        const result = filterXSSLINQExpression('test<script>alert("XSS")</script>');
 
-    expect(result).toEqual('test');
-})
+        expect(result).toEqual('test');
+    });
 
-test('filterXSSLINQExpression with escaped <a> attribute', () => {
-    const result = filterXSSLINQExpression('test\\<a onmouseover=\'alert(document.cookie)\'\\>xxs link\\</a\\>');
+    test('Escaped <a> attribute', () => {
+        const result = filterXSSLINQExpression('test\\<a onmouseover=\'alert(document.cookie)\'\\>xxs link\\</a\\>');
 
-    expect(result).toEqual('test\\<a>xxs link\\</a>');
-})
+        expect(result).toEqual('test\\<a>xxs link\\</a>');
+    });
+
+    test('XSS in LINQ expression', () => {
+        const result = filterXSSLINQExpression('ExternalCalibration.NextRecommendedDate < \"2024-10-29T02:53:47.647Z\" && ExternalCalibration.NextRecommendedDate > \"2025-10-29T08:53:47.647Z\" && Location.MinionId = \"e2etest-1730102822793-365e021a-d0c5-496c-87f8-8e4e5fa5090f\" && ExternalCalibration.NextRecommendedDate > \"2024-10-29T08:53:43.995Z\" && ExternalCalibration.NextRecommendedDate < \"\\<a onmouseover=\'alert(document.cookie)\'\\>xxs link\\</a\\>\"');
+
+        expect(result).toEqual('ExternalCalibration.NextRecommendedDate < \"2024-10-29T02:53:47.647Z\" && ExternalCalibration.NextRecommendedDate > \"2025-10-29T08:53:47.647Z\" && Location.MinionId = \"e2etest-1730102822793-365e021a-d0c5-496c-87f8-8e4e5fa5090f\" && ExternalCalibration.NextRecommendedDate > \"2024-10-29T08:53:43.995Z\" && ExternalCalibration.NextRecommendedDate < \"\\<a>xxs link\\\"</a>');
+    });
+});
 
 
-test('filterXSSLINQExpression with escaped <a> attribute', () => {
-    const result = filterXSSLINQExpression('ExternalCalibration.NextRecommendedDate < \"2024-10-29T02:53:47.647Z\" && ExternalCalibration.NextRecommendedDate > \"2025-10-29T08:53:47.647Z\" && Location.MinionId = \"e2etest-1730102822793-365e021a-d0c5-496c-87f8-8e4e5fa5090f\" && ExternalCalibration.NextRecommendedDate > \"2024-10-29T08:53:43.995Z\" && ExternalCalibration.NextRecommendedDate < \"\\<a onmouseover=\'alert(document.cookie)\'\\>xxs link\\</a\\>\"');
+describe("filterXSSField", () => {
+    test('simple field sanitization', () => {
+        const result = filterXSSField({ value: 'test<script>alert("XSS value")</script>', label: 'test<script>alert("XSS label")</script>' });
 
-    expect(result).toEqual('ExternalCalibration.NextRecommendedDate < \"2024-10-29T02:53:47.647Z\" && ExternalCalibration.NextRecommendedDate > \"2025-10-29T08:53:47.647Z\" && Location.MinionId = \"e2etest-1730102822793-365e021a-d0c5-496c-87f8-8e4e5fa5090f\" && ExternalCalibration.NextRecommendedDate > \"2024-10-29T08:53:43.995Z\" && ExternalCalibration.NextRecommendedDate < \"\\<a>xxs link\\\"</a>');
-})
-
-test('filterXSSField', () => {
-    const result = filterXSSField({ value: 'test<script>alert("XSS value")</script>', label: 'test<script>alert("XSS label")</script>'});
-
-    expect(result).toEqual({ value: 'test', label: 'test'});
-})
+        expect(result).toEqual({ value: 'test', label: 'test' });
+    });
+});
