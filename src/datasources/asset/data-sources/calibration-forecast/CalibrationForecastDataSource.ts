452d39df9a188e8e384e47805dcf1f038dc8b46f
NI\akerezsi <alex.kerezsi@ni.com>
2024-11-14 14:54:06 +0200

    Fix timezone related issues.

--- src/datasources/asset/data-sources/calibration-forecast/CalibrationForecastDataSource.ts
+++ src/datasources/asset/data-sources/calibration-forecast/CalibrationForecastDataSource.ts
@@ -66,8 +66,8 @@ export class CalibrationForecastDataSource extends AssetDataSourceBase {
 
     async processCalibrationForecastQuery(query: CalibrationForecastQuery, options: DataQueryRequest) {
         const result: DataFrameDTO = { refId: query.refId, fields: [] };
-        const from = options.range!.from.toISOString();
-        const to = options.range!.to.toISOString();
+        const from = options.range!.from.utc().toISOString();
+        const to = options.range!.to.utc().toISOString();
 
         const calibrationForecastResponse: CalibrationForecastResponse = await this.queryCalibrationForecast(
             query.groupBy,
@@ -102,6 +102,19 @@ export class CalibrationForecastDataSource extends AssetDataSourceBase {
     }
 
     processResultsGroupedByTime(result: DataFrameDTO, timeGrouping: AssetCalibrationForecastKey) {
+        const originalValuesMap = new Map<string, string>();
+        if (result.fields.length > 0) {
+            const field = result.fields[0];
+            const originalValues = field.values as string[] ?? [];
+            const formatter = this.forecastDateFormatterMap.get(field.name as AssetCalibrationForecastKey);
+            if (formatter) {
+                const formattedValues: string[] = field.values!.map(formatter);
+                for (let i = 0; i < originalValues!.length; i++) {
+                    originalValuesMap.set(formattedValues![i], originalValues![i]);
+                }
+            }
+        }
+
         result.fields.forEach(field => {
             field.name = this.createColumnNameFromDescriptor(field as FieldDTOWithDescriptor);
             const formatter = this.forecastDateFormatterMap.get(field.name as AssetCalibrationForecastKey);
@@ -111,17 +124,17 @@ export class CalibrationForecastDataSource extends AssetDataSourceBase {
             }
 
             if (!formatter) {
-                field.config = { links: this.createDataLinks(timeGrouping) };
+                field.config = { links: this.createDataLinks(timeGrouping, originalValuesMap) };
             }
         });
     }
 
-    private constructDataLinkBaseUrl(){
+    private constructDataLinkBaseUrl() {
         const pathname = locationService.getLocation().pathname;
-        return  pathname + '?orgId=${__org.id}&${__all_variables}';
+        return pathname + '?orgId=${__org.id}&${__all_variables}';
     }
 
-    private createDataLinks(timeGrouping: AssetCalibrationForecastKey): DataLink[] {
+    private createDataLinks(timeGrouping: AssetCalibrationForecastKey, originalValuesMap: Map<string, string>): DataLink[] {
         const url = this.constructDataLinkBaseUrl();
         return [
             {
@@ -130,32 +143,22 @@ export class CalibrationForecastDataSource extends AssetDataSourceBase {
                     if (!options.replaceVariables) {
                         return url;
                     }
-
                     const value = options.replaceVariables(`\${__data.fields.${timeGrouping}}`);
+                    const valueAsDate = new Date(originalValuesMap.get(value)!);
+
                     let from, to;
-
-                    const parseDate = (dateStr: string) => {
-                        const date = new Date(dateStr);
-                        date.setUTCHours(0);
-                        return date;
-                    };
-
                     switch (timeGrouping) {
                         case AssetCalibrationForecastKey.Day:
-                            const dayDate = parseDate(value);
-                            from = dayDate.valueOf();
-                            dayDate.setUTCDate(dayDate.getUTCDate() + 1);
-                            to = dayDate.valueOf();
+                            from = valueAsDate.valueOf();
+                            to = valueAsDate.setUTCDate(valueAsDate.getUTCDate() + 1).valueOf();
                             break;
                         case AssetCalibrationForecastKey.Week:
-                            [from, to] = value.split(' : ').map(dateStr => parseDate(dateStr).valueOf());
+                            from = valueAsDate.valueOf();
+                            to = valueAsDate.setUTCDate(valueAsDate.getUTCDate() + 6).valueOf();
                             break;
                         case AssetCalibrationForecastKey.Month:
-                            const monthDate = parseDate(value);
-                            monthDate.setUTCDate(monthDate.getUTCDate() + 1);
-                            from = monthDate.valueOf();
-                            monthDate.setUTCMonth(monthDate.getUTCMonth() + 1);
-                            to = monthDate.valueOf();
+                            from = valueAsDate.valueOf();
+                            to = valueAsDate.setUTCMonth(valueAsDate.getUTCMonth() + 1).valueOf();
                             break;
                     }
 
@@ -209,7 +212,7 @@ export class CalibrationForecastDataSource extends AssetDataSourceBase {
     }
 
     formatDateForMonth(date: string): string {
-        return new Date(date).toLocaleDateString('en-US', { year: 'numeric', month: 'long' });
+        return new Date(date).toLocaleDateString('en-US', { year: 'numeric', month: 'long', timeZone: 'UTC' });
     }
 
     async queryAssets(filter = '', take = -1): Promise<AssetModel[]> {
