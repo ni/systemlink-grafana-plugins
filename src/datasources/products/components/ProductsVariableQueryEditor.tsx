583fac2704b5f19c1afd1907307286402f6c6732
richie <richie.soshan@ni.com>
2025-02-17 18:17:39 +0530

    add family names support in query editor and query builder

--- src/datasources/products/components/ProductsVariableQueryEditor.tsx
+++ src/datasources/products/components/ProductsVariableQueryEditor.tsx
@@ -1,11 +1,10 @@
 import { QueryEditorProps } from "@grafana/data";
 import { ProductsDataSource } from "../ProductsDataSource";
-import { ProductVariableQuery, QBField } from "../types";
+import { ProductVariableQuery } from "../types";
 import { InlineField } from 'core/components/InlineField';
 import { ProductsQueryBuilder } from "./query-builder/ProductsQueryBuilder";
 import { Workspace } from "core/types";
-import React, { useState, useEffect, useMemo } from "react";
-import { ProductsQueryBuilderFields } from "../constants/ProductsQueryBuilder.constants";
+import React, { useState, useEffect } from "react";
 import { FloatingError } from "core/errors";
 
 type Props = QueryEditorProps<ProductsDataSource, ProductVariableQuery>;
@@ -39,26 +38,6 @@ export function ProductsVariableQueryEditor({ query, onChange, datasource }: Pro
         onChange({ ...query, queryBy: value });
     };
 
-    const FamilyField = useMemo(() => {
-        const familyField = ProductsQueryBuilderFields.FAMILY;
-        return {
-            ...familyField,
-            lookup: {
-                ...familyField.lookup,
-                dataSource: [
-                    ...familyNames.map(family => ({ label: family, value: family }))
-                ],
-                minLength: 1
-            }
-        }
-    }, [familyNames]);
-
-    const productsVariableQueryStaticFields: QBField[] = [
-        FamilyField,
-        ProductsQueryBuilderFields.NAME,
-        ProductsQueryBuilderFields.PROPERTIES
-    ];
-
     return (
         <>
             <InlineField label="Query By" labelWidth={10}>
@@ -67,8 +46,8 @@ export function ProductsVariableQueryEditor({ query, onChange, datasource }: Pro
                     onChange={(event: any) => onQueryByChange(event.detail.linq)}
                     workspaces={workspaces}
                     partNumbers={partNumbers}
+                    familyNames={familyNames}
                     globalVariableOptions={datasource.globalVariableOptions()}
-                    staticFields={productsVariableQueryStaticFields}
                 ></ProductsQueryBuilder>
             </InlineField>
             <FloatingError message={datasource.error} />
