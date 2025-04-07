bfb3f6fe84b5266beb9d8f6ff08f27514e4944a3
Ahalya Radhakrishnan <ahalya.radhakrishnan@ni.com>
2025-04-07 19:40:03 +0530

    removed onchange
    Signed-off-by: Ahalya Radhakrishnan <ahalya.radhakrishnan@ni.com>

--- src/datasources/results/components/ResultsQueryEditor.tsx
+++ src/datasources/results/components/ResultsQueryEditor.tsx
@@ -1,11 +1,10 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback } from 'react';
 import { QueryEditorProps, SelectableValue } from '@grafana/data';
 import { ResultsDataSource } from '../ResultsDataSource';
 import { QueryType, ResultsQuery } from '../types/types';
 import { QueryResultsEditor } from './editors/query-results/QueryResultsEditor';
 import { QueryResults } from '../types/QueryResults.types';
 import { InlineField, RadioButtonGroup, VerticalGroup } from '@grafana/ui';
-import { ResultsDefaults, StepsDefaults } from '../defaults';
 
 type Props = QueryEditorProps<ResultsDataSource, ResultsQuery>;
 
@@ -21,22 +20,12 @@ export function ResultsQueryEditor({ query, onChange, onRunQuery, datasource }: 
     },[onChange, onRunQuery]
   );
 
-  const handleQueryTypeChange = useCallback((value: QueryType): void => {
-    if (value === QueryType.Results) {
-      handleQueryChange({ ...query, queryType: QueryType.Results, ...ResultsDefaults}, true);
-    }
-    if (value === QueryType.Steps) {
-      handleQueryChange({ ...query, queryType: QueryType.Steps, ...StepsDefaults }, true);
-    }
-  }, [query, handleQueryChange]);
-
   return (
     <VerticalGroup>
       <InlineField label="Query Type" labelWidth={18}>
         <RadioButtonGroup
           options={Object.values(QueryType).map(value => ({ label: value, value })) as SelectableValue[]}
           value={query.queryType}
-          onChange={handleQueryTypeChange}
         />
       </InlineField>
       {query.queryType === QueryType.Results && (
