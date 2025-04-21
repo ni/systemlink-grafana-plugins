b61f6be5f2813108a4ab4962f028c058bfc0898b
Ahalya Radhakrishnan <ahalya.radhakrishnan@ni.com>
2025-04-21 15:45:52 +0530

    added query steps datasource
    Signed-off-by: Ahalya Radhakrishnan <ahalya.radhakrishnan@ni.com>

---
+++ src/datasources/results/query-handlers/query-steps/QueryStepsDataSource.test.ts
@@ -0,0 +1,194 @@
+import { MockProxy } from 'jest-mock-extended';
+import { BackendSrv, TemplateSrv } from '@grafana/runtime';
+import { createFetchError, createFetchResponse, getQueryBuilder, requestMatching, setupDataSource } from 'test/fixtures';
+import { Field } from '@grafana/data';
+import { QuerySteps, QueryStepsResponse, StepsProperties, StepsPropertiesOptions } from 'datasources/results/types/QuerySteps.types';
+import { OutputType, QueryType } from 'datasources/results/types/types';
+import { QueryStepsDataSource } from './QueryStepsDataSource';
+
+const mockQueryStepsResponse: QueryStepsResponse = {
+  steps: [
+    {
+      stepId: '1',
+      name: 'Step 1',
+      properties: {
+        key1: 'value1',
+        key2: 'value2',
+      },
+    },
+  ],
+  continuationToken: undefined,
+  totalCount: 1
+};
+
+let datastore: QueryStepsDataSource, backendServer: MockProxy<BackendSrv>, templateSrv: MockProxy<TemplateSrv>;
+
+describe('QueryStepsDataSource', () => {
+  beforeEach(() => {
+    [datastore, backendServer, templateSrv] = setupDataSource(QueryStepsDataSource);
+
+    backendServer.fetch
+      .calledWith(requestMatching({ url: '/nitestmonitor/v2/query-steps', method: 'POST' }))
+      .mockReturnValue(createFetchResponse(mockQueryStepsResponse));
+  })
+
+  describe('querySteps', () => {
+    test('returns data when there are valid queries', async () => {
+      const response = await datastore.querySteps();
+
+      expect(response).toMatchSnapshot();
+    });
+
+    test('raises an error returns API fails', async () => {
+      backendServer.fetch
+        .calledWith(requestMatching({ url: '/nitestmonitor/v2/query-steps' }))
+        .mockReturnValue(createFetchError(400));
+
+      await expect(datastore.querySteps())
+        .rejects
+        .toThrow('Request to url "/nitestmonitor/v2/query-steps" failed with status code: 400. Error message: "Error"');
+    });
+  });
+
+  describe('query', () => {
+    test('returns data for valid data-output-type query', async () => {
+      const query = buildQuery({
+        refId: 'A',
+        outputType: OutputType.Data
+      });
+
+      const response = await datastore.query(query);
+
+      expect(response.data).toHaveLength(1);
+      expect(response.data).toMatchSnapshot();
+    });
+
+    test('returns total count for valid total count output type queries', async () => {
+      const query = buildQuery({
+        refId: 'A',
+        outputType: OutputType.TotalCount,
+      });
+
+      const response = await datastore.query(query);
+
+      expect(response.data).toHaveLength(1);
+      expect(response.data).toMatchSnapshot();
+    });
+
+    test('returns no data when QuerySteps API returns empty array', async () => {
+      backendServer.fetch
+        .calledWith(requestMatching({ url: '/nitestmonitor/v2/query-steps', method: 'POST' }))
+        .mockReturnValue(
+          createFetchResponse(
+            {
+              steps: [],
+              continuationToken: null,
+              totalCount: 0
+            } as unknown as QueryStepsResponse
+          )
+        );
+
+      const query = buildQuery();
+      const response = await datastore.query(query);
+
+      expect(response.data).toMatchSnapshot();
+    });
+
+    test('returns no data when Query Steps returns error', async () => {
+        backendServer.fetch
+          .calledWith(requestMatching({ url: '/nitestmonitor/v2/query-steps' }))
+          .mockReturnValue(createFetchError(400));
+
+        const query = buildQuery(
+          {
+            refId: 'A',
+            outputType: OutputType.Data
+          },
+        );
+
+        await expect(datastore.query(query))
+        .rejects
+        .toThrow('Request to url "/nitestmonitor/v2/query-steps" failed with status code: 400. Error message: "Error"');
+    });
+
+    test('should convert properties to Grafana fields', async () => {
+        const query = buildQuery(
+          {
+            refId: 'A',
+            outputType: OutputType.Data,
+            properties: [ StepsPropertiesOptions.PROPERTIES as StepsProperties ]
+          },
+        );
+
+        const response = await datastore.query(query);
+
+        const fields = response.data[0].fields as Field[];
+        expect(fields).toMatchSnapshot();
+    });
+
+    test('includes templateSrv replaced values in the filter', async () => {
+      const timeRange = {
+        Started: 'startedAt',
+        Updated: 'updatedAt',
+      }
+      const selectedUseTimeRangeFor = 'Started';
+      const filter = `(${timeRange[selectedUseTimeRangeFor]} > "\${__from:date}" && ${timeRange[selectedUseTimeRangeFor]} < "\${__to:date}")`;
+      const replacedFilter = `(${timeRange[selectedUseTimeRangeFor]} > "2025-04-01" && ${timeRange[selectedUseTimeRangeFor]} < "2025-04-02")`;
+      templateSrv.replace.calledWith().mockReturnValue(replacedFilter);
+
+        const query = buildQuery(
+          {
+            refId: 'A',
+            outputType: OutputType.Data,
+            useTimeRange: true,
+            useTimeRangeFor: selectedUseTimeRangeFor
+          },
+        );
+
+      await datastore.query(query);
+
+      expect(templateSrv.replace).toHaveBeenCalledWith(filter, expect.anything());
+      expect(backendServer.fetch).toHaveBeenCalledWith(
+        expect.objectContaining({
+          data: expect.objectContaining({ filter: replacedFilter }),
+        })
+      );
+    });
+
+    test('should handle null and undefined properties', async () => {
+        backendServer.fetch
+          .calledWith(requestMatching({ url: '/nitestmonitor/v2/query-steps' }))
+          .mockReturnValue(createFetchResponse({
+            steps: [
+              {
+                id: '1',
+                properties: null
+              }
+            ], continuationToken: null, totalCount: 1
+          } as unknown as QueryStepsResponse));
+
+        const query = buildQuery(
+          {
+            refId: 'A',
+            outputType: OutputType.Data,
+            properties: [
+              StepsPropertiesOptions.PROPERTIES
+            ] as StepsProperties[],
+            orderBy: undefined
+          },
+        );
+
+        const response = await datastore.query(query);
+        const fields = response.data[0].fields as Field[];
+        expect(fields).toEqual([
+          { name: 'properties', values: [""], type: 'string' },
+        ]);
+    });
+  });
+
+  const buildQuery = getQueryBuilder<QuerySteps>()({
+    refId: 'A',
+    queryType: QueryType.Steps,
+    outputType: OutputType.Data
+  });
+});
---
+++ src/datasources/results/query-handlers/query-steps/QueryStepsDataSource.ts
@@ -0,0 +1,103 @@
+import { DataQueryRequest, DataFrameDTO, FieldType } from '@grafana/data';
+import { OutputType } from 'datasources/results/types/types';
+import {
+  QuerySteps,
+  QueryStepsResponse,
+  StepsProperties,
+  StepsPropertiesOptions,
+  StepsResponseProperties,
+} from 'datasources/results/types/QuerySteps.types';
+import { ResultsDataSourceBase } from 'datasources/results/ResultsDataSourceBase';
+import { defaultStepsQuery } from 'datasources/results/defaultQueries';
+
+export class QueryStepsDataSource extends ResultsDataSourceBase {
+  queryStepsUrl = this.baseUrl + '/v2/query-steps';
+
+  defaultQuery = defaultStepsQuery;
+
+  async querySteps(
+    filter?: string,
+    orderBy?: string,
+    projection?: StepsProperties[],
+    take?: number,
+    descending?: boolean,
+    returnCount = false
+  ): Promise<QueryStepsResponse> {
+    try {
+      return await this.post<QueryStepsResponse>(`${this.queryStepsUrl}`, {
+        filter,
+        orderBy,
+        descending,
+        projection,
+        take,
+        returnCount,
+      });
+    } catch (error) {
+      throw new Error(`An error occurred while querying steps: ${error}`);
+    }
+  }
+
+  async runQuery(query: QuerySteps, options: DataQueryRequest): Promise<DataFrameDTO> {
+    const responseData = await this.querySteps(
+      this.getTimeRangeFilter(options, query.useTimeRange, query.useTimeRangeFor),
+      query.orderBy,
+      query.properties,
+      query.recordCount,
+      query.descending,
+      true
+    );
+
+    if (responseData.steps.length === 0) {
+      return {
+        refId: query.refId,
+        fields: [],
+      };
+    }
+
+    if (query.outputType === OutputType.Data) {
+      const stepsResponse = responseData.steps;
+      const stepResponseKeys = new Set(Object.keys(stepsResponse[0]));
+      const selectedFields = (query.properties || []).filter(field => stepResponseKeys.has(field));
+      const fields = this.processFields(selectedFields, stepsResponse);
+
+      return {
+        refId: query.refId,
+        fields: fields,
+      };
+    } else {
+      return {
+        refId: query.refId,
+        fields: [{ name: 'Total count', values: [responseData.totalCount] }],
+      };
+    }
+  }
+
+  private processFields(selectedFields: StepsProperties[], stepsResponse: StepsResponseProperties[]): any[] {
+    return selectedFields.map(field => {
+      const isTimeField = field === StepsPropertiesOptions.UPDATED_AT || field === StepsPropertiesOptions.STARTED_AT;
+
+      const fieldType = isTimeField ? FieldType.time : FieldType.string;
+      const values = stepsResponse.map(data => data[field as keyof StepsResponseProperties]);
+
+      switch (field) {
+        case StepsPropertiesOptions.PROPERTIES:
+        case StepsPropertiesOptions.INPUTS:
+        case StepsPropertiesOptions.OUTPUTS:
+        case StepsPropertiesOptions.DATA:
+          return {
+            name: field,
+            values: values.map(value => (value !== null ? JSON.stringify(value) : '')),
+            type: fieldType,
+          };
+        case StepsPropertiesOptions.STATUS:
+          return { name: field, values: values.map((value: any) => value?.statusType), type: fieldType };
+        default:
+          return { name: field, values, type: fieldType };
+      }
+    });
+  }
+
+  shouldRunQuery(_: QuerySteps): boolean {
+    return true;
+  }
+}
