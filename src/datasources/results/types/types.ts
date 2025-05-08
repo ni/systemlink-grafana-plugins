b0f66f71c9ce0e8f0e5c78b321b9664157b1a5b1
Ahalya Radhakrishnan <ahalya.radhakrishnan@ni.com>
2025-05-08 18:24:05 +0530

    removed status enum
    Signed-off-by: Ahalya Radhakrishnan <ahalya.radhakrishnan@ni.com>

--- src/datasources/results/types/types.ts
+++ src/datasources/results/types/types.ts
@@ -32,17 +32,3 @@ export const ResultsFeatureTogglesDefaults: ResultsFeatureToggles = {
   queryByResults: true,
   queryBySteps: true
 }
-
-export enum TestMeasurementStatus {
-  Done = 'Done',
-  Errored = 'Errored',
-  Failed = 'Failed',
-  Passed = 'Passed',
-  Skipped = 'Skipped',
-  Terminated = 'Terminated',
-  TimedOut = 'Timed out',
-  Custom = 'Custom',
-  Looping = 'Looping',
-  Running = 'Running',
-  Waiting = 'Waiting',
-}
