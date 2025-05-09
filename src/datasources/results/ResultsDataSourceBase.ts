8d2c38d71644193f57690fb183088e611541f11d
Ahalya Radhakrishnan <ahalya.radhakrishnan@ni.com>
2025-05-09 22:43:33 +0530

    moved partnumber cache
    Signed-off-by: Ahalya Radhakrishnan <ahalya.radhakrishnan@ni.com>

--- src/datasources/results/ResultsDataSourceBase.ts
+++ src/datasources/results/ResultsDataSourceBase.ts
@@ -6,6 +6,7 @@ import { QueryBuilderOption, Workspace } from "core/types";
 import { getVariableOptions } from "core/utils";
 import { ExpressionTransformFunction } from "core/query-builder.utils";
 import { QueryBuilderOperations } from "core/query-builder.constants";
+import { ResultsPropertiesOptions } from "./types/QueryResults.types";
 
 export abstract class ResultsDataSourceBase extends DataSourceBase<ResultsQuery> {
   baseUrl = this.instanceSettings.url + '/nitestmonitor';
