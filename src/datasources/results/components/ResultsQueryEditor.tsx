c4aa013688d72e1d56e7141562b8f40b22d03b5b
Ahalya Radhakrishnan <ahalya.radhakrishnan@ni.com>
2025-05-10 01:27:17 +0530

    after alignment
    Signed-off-by: Ahalya Radhakrishnan <ahalya.radhakrishnan@ni.com>

--- src/datasources/results/components/ResultsQueryEditor.tsx
+++ src/datasources/results/components/ResultsQueryEditor.tsx
@@ -41,7 +41,7 @@ export function ResultsQueryEditor({ query, onChange, onRunQuery, datasource }: 
   }, [query, handleQueryChange]);
 
   return (
-    <VerticalGroup>
+    <>
       <InlineField label="Query Type" labelWidth={25} tooltip={tooltips.queryType}>
         <RadioButtonGroup
           options={Object.values(QueryType).map(value => ({ label: value, value })) as SelectableValue[]}
@@ -63,7 +63,7 @@ export function ResultsQueryEditor({ query, onChange, onRunQuery, datasource }: 
           datasource={datasource.queryStepsDataSource}
         />
       )}
-    </VerticalGroup>
+    </>
   );
 }
 
