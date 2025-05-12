fb50a96d9c966f2bf55981555ddbbc0a6ab2425e
priya-ni <priyadarshini.gopal@emerson.com>
2025-05-12 15:11:09 +0000

    add output type to testplan

--- src/datasources/test-plans/types.ts
+++ src/datasources/test-plans/types.ts
@@ -1,4 +1,10 @@
 import { DataQuery } from '@grafana/schema'
 
 export interface TestPlansQuery extends DataQuery {
+    outputType: OutputType;
+}
+
+export enum OutputType {
+    Properties = "Properties",
+    TotalCount = "Total Count"
 }
