7fdd2b3afb95f7ab9b9337b940976e9ed4120b2e
Ahalya Radhakrishnan <ahalya.radhakrishnan@ni.com>
2025-05-12 15:31:23 +0530

    refactor query filters in runquery
    Signed-off-by: Ahalya Radhakrishnan <ahalya.radhakrishnan@ni.com>

--- src/datasources/results/constants/StepsQueryBuilder.constants.ts
+++ src/datasources/results/constants/StepsQueryBuilder.constants.ts
@@ -16,6 +16,7 @@ export const StepsQueryBuilderFields: Record<string, QBField> = {
   KEYWORDS: {
     label: 'Step keywords',
     dataField: StepsQueryBuilderFieldNames.KEYWORDS,
+    dataType: 'Array',
     filterOperations: [
       QueryBuilderOperations.LIST_EQUALS.name,
       QueryBuilderOperations.LIST_DOES_NOT_EQUAL.name,
