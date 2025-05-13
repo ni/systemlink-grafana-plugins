94c783f393df76529d92f2492f79a405fb967928
priya-ni <priyadarshini.gopal@emerson.com>
2025-05-13 14:34:29 +0000

    add proeprties to test plans

--- src/datasources/test-plans/components/TestPlansQueryEditor.tsx
+++ src/datasources/test-plans/components/TestPlansQueryEditor.tsx
@@ -1,32 +1,65 @@
 import React, { useCallback } from 'react';
 import { QueryEditorProps, SelectableValue } from '@grafana/data';
 import { TestPlansDataSource } from '../TestPlansDataSource';
-import { OutputType, TestPlansQuery } from '../types';
-import { InlineField, RadioButtonGroup } from '@grafana/ui';
+import { OutputType, Properties, TestPlansQuery } from '../types';
+import { InlineField, MultiSelect, RadioButtonGroup, VerticalGroup } from '@grafana/ui';
 
 type Props = QueryEditorProps<TestPlansDataSource, TestPlansQuery>;
 
 export function TestPlansQueryEditor({ query, onChange, onRunQuery, datasource }: Props) {
   query = datasource.prepareQuery(query);
 
-  const onOutputTypeChange = useCallback((value: OutputType) => {
-    onChange({ ...query, outputType: value });
-    onRunQuery();
-  }, [query, onChange, onRunQuery]);
+  const handleQueryChange = useCallback(
+    (query: TestPlansQuery, runQuery = true): void => {
+      onChange(query);
+      if (runQuery) {
+        onRunQuery();
+      }
+    }, [onChange, onRunQuery]
+  );
+
+  const onOutputTypeChange = (value: OutputType) => {
+    handleQueryChange({ ...query, outputType: value });
+  };
+
+  const onPropertiesChange = (items: Array<SelectableValue<string>>) => {
+    if (items !== undefined) {
+      handleQueryChange({ ...query, properties: items.map(i => i.value as Properties) });
+    }
+  };
 
   return (
     <>
-      <InlineField label="Output" labelWidth={14} tooltip={tooltips.outputType}>
-        <RadioButtonGroup
-          options={Object.values(OutputType).map(value => ({ label: value, value })) as SelectableValue[]}
-          onChange={onOutputTypeChange}
-          value={query.outputType}
-        />
-      </InlineField>
+      <VerticalGroup>
+        <InlineField label="Output" labelWidth={18} tooltip={tooltips.outputType}>
+          <RadioButtonGroup
+            options={Object.values(OutputType).map(value => ({ label: value, value })) as SelectableValue[]}
+            onChange={onOutputTypeChange}
+            value={query.outputType}
+          />
+        </InlineField>
+        {query.outputType === OutputType.Properties && (
+          <InlineField label="Properties" labelWidth={18} tooltip={tooltips.properties}>
+            <MultiSelect
+              placeholder="Select properties to query"
+              options={Object.entries(Properties).map(([key, value]) => ({ label: value, value: key })) as SelectableValue[]}
+              onChange={onPropertiesChange}
+              value={query.properties}
+              defaultValue={query.properties}
+              noMultiValueWrap={true}
+              maxVisibleValues={5}
+              width={60}
+              allowCustomValue={false}
+              closeMenuOnSelect={false}
+            />
+          </InlineField>
+        )}
+      </VerticalGroup>
     </>
   );
 }
 
 const tooltips = {
-  outputType: 'This field specifies the output type to fetch test plan properties or total count'
+  outputType: 'This field specifies the output type to fetch test plan properties or total count.',
+  properties: "Specifies the properties to be queried."
 };
