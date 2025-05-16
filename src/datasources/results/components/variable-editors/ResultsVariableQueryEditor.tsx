5425a9a0efe5ab6bd4977984f985e5000af91a49
richie <richie.soshan@ni.com>
2025-05-16 14:23:23 +0530

    feat(results): Enhance ResultsVariableQueryEditor with workspace and part number loading

--- src/datasources/results/components/variable-editors/ResultsVariableQueryEditor.tsx
+++ src/datasources/results/components/variable-editors/ResultsVariableQueryEditor.tsx
@@ -1,40 +1,62 @@
 import { QueryEditorProps, SelectableValue } from '@grafana/data';
 import { InlineField } from 'core/components/InlineField';
-import React from 'react';
-import { QueryResultsDataSource } from 'datasources/results/query-handlers/query-results/QueryResultsDataSource';
+import React, { useEffect, useRef, useState } from 'react';
 import { ResultsVariableProperties, ResultsVariableQuery } from 'datasources/results/types/QueryResults.types';
 import { ResultsQueryBuilder } from '../query-builders/query-results/ResultsQueryBuilder';
 import { Select } from '@grafana/ui';
+import { Workspace } from 'core/types';
+import { enumToOptions } from 'core/utils';
+import { ResultsDataSourceOptions, ResultsQuery, TestMeasurementStatus } from 'datasources/results/types/types';
+import { ResultsDataSource } from 'datasources/results/ResultsDataSource';
 
-type Props = QueryEditorProps<QueryResultsDataSource, ResultsVariableQuery>;
+type Props = QueryEditorProps<ResultsDataSource, ResultsQuery, ResultsDataSourceOptions>;
 
 export function ResultsVariableQueryEditor({ query, onChange, datasource }: Props) {
+  const [workspaces, setWorkspaces] = useState<Workspace[]>([]);
+  const [partNumbers, setPartNumbers] = useState<string[]>([]);
+  const queryResultsquery = query as ResultsVariableQuery;
+  const queryResultsDataSource = useRef(datasource.queryResultsDataSource);
+
+  useEffect(() => { 
+    const loadWorkspaces = async () => {
+      await queryResultsDataSource.current.loadWorkspaces();
+      setWorkspaces(Array.from(queryResultsDataSource.current.workspacesCache.values()));
+    };
+    const loadPartNumbers = async () => {
+      await queryResultsDataSource.current.getPartNumbers();
+      setPartNumbers(queryResultsDataSource.current.partNumbersCache);
+    };
+
+    loadWorkspaces();
+    loadPartNumbers();
+  }, [datasource]);
+
   const onPropertiesChange = (item: SelectableValue<string>) => {
-    onChange({ ...query, properties: item.value });
+    onChange({ ...queryResultsquery, properties: item.value } as ResultsVariableQuery);
   };
 
   const onQueryByChange = (value: string) => {
-    onChange({ ...query, queryBy: value });
+    onChange({ ...queryResultsquery, queryBy: value } as ResultsVariableQuery);
   };
 
   return (
     <>
       <InlineField label="Properties" labelWidth={12} tooltip={tooltips.properties}>
-        <Select 
-            onChange={onPropertiesChange}
-            options={ResultsVariableProperties as SelectableValue[]}
-            value={query.properties}
-            defaultValue={query.properties}
+        <Select
+          onChange={onPropertiesChange}
+          options={ResultsVariableProperties as SelectableValue[]}
+          value={queryResultsquery.properties}
+          defaultValue={queryResultsquery.properties}
         ></Select>
       </InlineField>
       <InlineField label="Query By" labelWidth={12} tooltip={tooltips.queryBy}>
         <ResultsQueryBuilder
-          filter={query.queryBy}
+          filter={queryResultsquery.queryBy}
           onChange={(event: any) => onQueryByChange(event.detail.linq)}
-          workspaces={[]}
-          partNumbers={[]}
-          status={[]}
-          globalVariableOptions={[]}
+          workspaces={workspaces}
+          partNumbers={partNumbers}
+          status={enumToOptions(TestMeasurementStatus).map(option => option.value as string)}
+          globalVariableOptions={queryResultsDataSource.current.globalVariableOptions()}
         ></ResultsQueryBuilder>
       </InlineField>
     </>
@@ -43,5 +65,5 @@ export function ResultsVariableQueryEditor({ query, onChange, datasource }: Prop
 
 const tooltips = {
   queryBy: 'This field applies a filter to the query results.',
-  properties: "This field specifies the properties to use in the query.",
+  properties: 'This field specifies the properties to use in the query.',
 };
