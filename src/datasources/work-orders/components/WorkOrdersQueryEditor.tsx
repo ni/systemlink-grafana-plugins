9d7b69636ff35df5e956689e4a7998ccbd6b4523
Manisha Sivakumar <manisha.sivakumar@emerson.com>
2025-05-16 10:18:22 +0000

    add output type
    Signed-off-by: Manisha Sivakumar <manisha.sivakumar@emerson.com>

--- src/datasources/work-orders/components/WorkOrdersQueryEditor.tsx
+++ src/datasources/work-orders/components/WorkOrdersQueryEditor.tsx
@@ -1,15 +1,32 @@
-import React from 'react';
-import { QueryEditorProps } from '@grafana/data';
+import React, { useCallback } from 'react';
+import { QueryEditorProps, SelectableValue } from '@grafana/data';
 import { WorkOrdersDataSource } from '../WorkOrdersDataSource';
-import { WorkOrdersQuery } from '../types';
+import { OutputType, WorkOrdersQuery } from '../types';
+import { InlineField, RadioButtonGroup } from '@grafana/ui';
 
 type Props = QueryEditorProps<WorkOrdersDataSource, WorkOrdersQuery>;
 
-export function WorkOrdersQueryEditor({ query, onChange, onRunQuery }: Props) {
+export function WorkOrdersQueryEditor({ query, onChange, onRunQuery, datasource }: Props) {
+  query = datasource.prepareQuery(query);
+
+  const onOutputTypeChange = useCallback((value: OutputType) => {
+    onChange({ ...query, outputType: value });
+    onRunQuery();
+  }, [query, onChange, onRunQuery]);
 
   return (
     <>
-    <span>Placeholder for querybuilder</span>
+      <InlineField label="Output" labelWidth={14} tooltip={tooltips.outputType}>
+        <RadioButtonGroup
+          options={Object.values(OutputType).map(value => ({ label: value, value })) as SelectableValue[]}
+          onChange={onOutputTypeChange}
+          value={query.outputType}
+        />
+      </InlineField>
     </>
   );
 }
+
+const tooltips = {
+  outputType: 'This field specifies the output type to fetch test plan properties or total count'
+};
