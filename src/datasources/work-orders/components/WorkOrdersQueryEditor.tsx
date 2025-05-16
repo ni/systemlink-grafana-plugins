e9f755bda90a17aee5a03122e84eaadf516fd55d
Manisha Sivakumar <manisha.sivakumar@emerson.com>
2025-05-16 10:45:21 +0000

    add label
    Signed-off-by: Manisha Sivakumar <manisha.sivakumar@emerson.com>

--- src/datasources/work-orders/components/WorkOrdersQueryEditor.tsx
+++ src/datasources/work-orders/components/WorkOrdersQueryEditor.tsx
@@ -3,15 +3,24 @@ import { QueryEditorProps } from '@grafana/data';
 import { WorkOrdersDataSource } from '../WorkOrdersDataSource';
 import { WorkOrdersQuery } from '../types';
 import { WorkOrdersQueryBuilder } from './query-builder/WorkOrdersQueryBuilder';
+import { InlineField, VerticalGroup } from '@grafana/ui';
 
 type Props = QueryEditorProps<WorkOrdersDataSource, WorkOrdersQuery>;
 
 export function WorkOrdersQueryEditor({ query, onChange, onRunQuery }: Props) {
   return (
     <>
-      <WorkOrdersQueryBuilder 
-        globalVariableOptions={[]}
-      ></WorkOrdersQueryBuilder>
+      <VerticalGroup>
+        <InlineField label="Query By" labelWidth={25} tooltip={tooltips.queryBy}>
+          <WorkOrdersQueryBuilder 
+            globalVariableOptions={[]}
+          ></WorkOrdersQueryBuilder>
+        </InlineField>
+      </VerticalGroup>
     </>
   );
 }
+
+const tooltips = {
+  queryBy: 'This optional field applies a filter to query workorders.',
+};
