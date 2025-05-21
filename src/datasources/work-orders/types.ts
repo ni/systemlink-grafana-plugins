ca2717e98e00ae1a3b0b6ad075165fa6894e8752
Manisha Sivakumar <manisha.sivakumar@emerson.com>
2025-05-21 12:28:35 +0000

    query workorders
    Signed-off-by: Manisha Sivakumar <manisha.sivakumar@emerson.com>

--- src/datasources/work-orders/types.ts
+++ src/datasources/work-orders/types.ts
@@ -4,6 +4,9 @@ export interface WorkOrdersQuery extends DataQuery {
   queryBy?: string;
   outputType: OutputType;
   properties?: WorkOrderPropertiesOptions[];
+  orderBy?: string;
+  descending?: boolean;
+  take?: number;
 }
 
 export enum OutputType {
@@ -106,3 +109,55 @@ export const WorkOrderProperties = {
     field: 'properties',
   },
 } as const;
+
+export interface QueryWorkOrdersRequestBody {
+  filter?: string;
+  take?: number;
+  orderBy?: string;
+  descending?: boolean;
+  returnCount?: boolean;
+  projection?: string[];
+  substitutions?: string[];
+  continuationToken?: string;
+}
+
+export interface WorkOrdersResponse {
+  workOrders: WorkOrder[];
+  continuationToken?: string;
+  totalCount?: number;
+}
+
+export interface WorkOrder {
+  id: string;
+  name: string;
+  type: Type;
+  description: string | null;
+  state: State;
+  createdBy: string;
+  updatedBy: string;
+  assignedTo: string | null;
+  requestedBy: string | null;
+  createdAt: string;
+  updatedAt: string;
+  earliestStartDate: string | null;
+  dueDate: string | null;
+  properties: {
+    [key: string]: string;
+  };
+  workspace: string;
+}
+
+export enum Type {
+  TestRequest = 'TEST_REQUEST',
+}
+
+export enum State {
+  New = 'NEW',
+  Defined = 'DEFINED',
+  Reviewed = 'REVIEWED',
+  Scheduled = 'SCHEDULED',
+  InProgress = 'IN_PROGRESS',
+  PendingApproval = 'PENDING_APPROVAL',
+  Closed = 'CLOSED',
+  Canceled = 'CANCELED',
+}
