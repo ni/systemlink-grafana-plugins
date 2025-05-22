29da948ae288f72fc1eab3e00e9aea2b4e4ab2a7
Manisha Sivakumar <manisha.sivakumar@emerson.com>
2025-05-22 11:02:38 +0000

    remove properties tooltip
    Signed-off-by: Manisha Sivakumar <manisha.sivakumar@emerson.com>

--- src/datasources/work-orders/components/WorkOrdersQueryEditor.tsx
+++ src/datasources/work-orders/components/WorkOrdersQueryEditor.tsx
@@ -84,7 +84,6 @@ export function WorkOrdersQueryEditor({ query, onChange, onRunQuery, datasource 
 const tooltips = {
   queryBy: 'This optional field specifies the query filters.',
   outputType: 'This field specifies the output type to fetch work order properties or total count',
-  properties: 'This field specifies the properties to use in the query.',
   orderBy: 'This field specifies the query order of the work orders.',
   descending: 'This toggle returns the work orders query in descending order.',
 };
