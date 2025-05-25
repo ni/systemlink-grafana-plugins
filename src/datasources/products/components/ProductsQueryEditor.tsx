f1c5a4f39ff53a70a414d8dd35b4b667b8a27027
richie <richie.soshan@ni.com>
2025-05-26 00:30:15 +0530

    fix(ProductsQueryEditor): Standardize string quotes in tooltips and input type

--- src/datasources/products/components/ProductsQueryEditor.tsx
+++ src/datasources/products/components/ProductsQueryEditor.tsx
@@ -127,7 +127,7 @@ export function ProductsQueryEditor({ query, onChange, onRunQuery, datasource }:
               <AutoSizeInput
                 minWidth={26}
                 maxWidth={26}
-                type="number"
+                type='number'
                 defaultValue={query.recordCount}
                 onCommitChange={recordCountChange}
                 placeholder="Enter record count"
@@ -145,9 +145,9 @@ export function ProductsQueryEditor({ query, onChange, onRunQuery, datasource }:
 }
 
 const tooltips = {
-  properties: 'Specifies the properties to be queried.',
-  recordCount: 'Specifies the maximum number of products to return.',
-  orderBy: 'Specifies the field to order the queried products by.',
-  descending: 'Specifies whether to return the products in descending order.',
+  properties: "Specifies the properties to be queried.",
+  recordCount: "Specifies the maximum number of products to return.",
+  orderBy: "Specifies the field to order the queried products by.",
+  descending: "Specifies whether to return the products in descending order.",
   queryBy: 'Specifies the filter to be applied on the queried products. This is an optional field.',
 };
