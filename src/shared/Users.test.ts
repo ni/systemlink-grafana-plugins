aaf11941fbb3ea37b7e59baec2c92e9be701485b
Manisha Sivakumar <manisha.sivakumar@emerson.com>
2025-05-30 10:20:20 +0000

    fix test
    Signed-off-by: Manisha Sivakumar <manisha.sivakumar@emerson.com>

--- src/shared/Users.test.ts
+++ src/shared/Users.test.ts
@@ -45,8 +45,33 @@ describe('Users', () => {
   let users: Users;
 
   beforeEach(() => {
-    jest.clearAllMocks();
     users = new Users(mockInstanceSettings, mockBackendSrv);
+  });
+
+  describe('usersCache', () => {
+    it('should fetch and cache users', async () => {
+      const result = await users.usersCache;
+
+      expect(result).toEqual(mockUsers);
+      expect(queryUntilComplete).toHaveBeenCalledTimes(1);
+      expect(queryUntilComplete).toHaveBeenCalledWith(expect.any(Function), {
+        maxTakePerRequest: QUERY_USERS_MAX_TAKE,
+        requestsPerSecond: QUERY_USERS_REQUEST_PER_SECOND,
+      });
+
+      const cachedUsers = await users.usersCache;
+      expect(cachedUsers).toEqual(mockUsers);
+      expect(queryUntilComplete).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('usersMapCache', () => {
+    it('should generate a map of user IDs to full names', async () => {
+      const userMap = await users.usersMapCache;
+
+      expect(userMap.get('1')).toBe('John Doe');
+      expect(userMap.get('2')).toBe('Jane Smith');
+    });
   });
 
   describe('getUserFullName', () => {
@@ -60,44 +85,6 @@ describe('Users', () => {
     it('should return the full name and email of a user', () => {
       const result = Users.getUserNameAndEmail(mockUsers[0]);
       expect(result).toBe('John Doe (john.doe@example.com)');
-    });
-  });
-
-  describe('usersCache', () => {
-    it('should fetch and cache users', async () => {
-      const result = await users.usersCache;
-
-      expect(result).toEqual(mockUsers);
-      expect(queryUntilComplete).toHaveBeenCalledTimes(1);
-      expect(queryUntilComplete).toHaveBeenCalledWith(expect.any(Function), {
-        maxTakePerRequest: QUERY_USERS_MAX_TAKE,
-        requestsPerSecond: QUERY_USERS_REQUEST_PER_SECOND,
-      });
-    });
-
-    it('should return cached users on subsequent calls', async () => {
-      await users.usersCache; // First call to populate cache
-      const cachedUsers = await users.usersCache;
-
-      expect(cachedUsers).toEqual(mockUsers);
-      expect(queryUntilComplete).toHaveBeenCalledTimes(1);
-    });
-  });
-
-  describe('usersMapCache', () => {
-    it('should generate a map of user IDs to full names', async () => {
-      const userMap = await users.usersMapCache;
-
-      expect(userMap.get('1')).toBe('John Doe');
-      expect(userMap.get('2')).toBe('Jane Smith');
-    });
-
-    it('should return cached map', async () => {
-      await users.usersMapCache; // First call to populate cache
-      const userMap = await users.usersMapCache;
-
-      expect(userMap.get('1')).toBe('John Doe');
-      expect(userMap.get('2')).toBe('Jane Smith');
     });
   });
 });
