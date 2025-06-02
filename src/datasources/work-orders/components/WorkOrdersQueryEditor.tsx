86c601c8f8d4a29bea3865ee01b4fa12d2bf3817
Manisha Sivakumar <manisha.sivakumar@emerson.com>
2025-06-02 09:30:51 +0000

    add variable editor
    Signed-off-by: Manisha Sivakumar <manisha.sivakumar@emerson.com>

--- src/datasources/work-orders/components/WorkOrdersQueryEditor.tsx
+++ src/datasources/work-orders/components/WorkOrdersQueryEditor.tsx
@@ -11,6 +11,7 @@ import {
   VerticalGroup
 } from '@grafana/ui';
 import './WorkOrdersQueryEditor.scss';
+import { tooltips } from '../constants/QueryEditor.constants';
 
 type Props = QueryEditorProps<WorkOrdersDataSource, WorkOrdersQuery>;
 
@@ -122,11 +123,3 @@ export function WorkOrdersQueryEditor({ query, onChange, onRunQuery, datasource 
     </>
   );
 }
-
-const tooltips = {
-  queryBy: 'This optional field specifies the query filters.',
-  outputType: 'This field specifies the output type to fetch work order properties or total count',
-  properties: 'This field specifies the properties to use in the query.',
-  orderBy: 'This field specifies the query order of the work orders.',
-  descending: 'This toggle returns the work orders query in descending order.',
-};
