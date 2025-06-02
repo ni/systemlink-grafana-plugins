a93125645ed4b70f73cdd54336e6545521602b90
Manisha Sivakumar <manisha.sivakumar@emerson.com>
2025-06-02 09:08:50 +0000

    add methods to utils
    Signed-off-by: Manisha Sivakumar <manisha.sivakumar@emerson.com>

--- src/core/utils.ts
+++ src/core/utils.ts
@@ -3,6 +3,8 @@ import { useAsync } from 'react-use';
 import { DataSourceBase } from './DataSourceBase';
 import { BatchQueryConfig, QueryResponse, SystemLinkError, Workspace } from './types';
 import { TemplateSrv } from '@grafana/runtime';
+import { QueryBuilderOperations } from './query-builder.constants';
+import { ExpressionTransformFunction } from './query-builder.utils';
 
 export function enumToOptions<T>(stringEnum: { [name: string]: T }): Array<SelectableValue<T>> {
   const RESULT = [];
@@ -39,7 +41,7 @@ export function useWorkspaceOptions<DSType extends DataSourceBase<any, any>>(dat
   return useAsync(async () => {
     const workspaces = await datasource.getWorkspaces();
     const workspaceOptions = workspaces.map(w => ({ label: w.name, value: w.id }));
-    workspaceOptions?.unshift(...getVariableOptions(datasource))
+    workspaceOptions?.unshift(...getVariableOptions(datasource));
     return workspaceOptions;
   });
 }
@@ -72,7 +74,8 @@ export function replaceVariables(values: string[], templateSrv: TemplateSrv) {
   const replaced: string[] = [];
   values.forEach(value => {
     if (templateSrv.containsTemplate(value)) {
-      const variableReplacedValues = templateSrv.replace(value) // Replace variable with their values
+      const variableReplacedValues = templateSrv
+        .replace(value) // Replace variable with their values
         .replace(/[{}]/g, '') // return values without curly braces for multi-value variables which are returned as {value1,value2}
         .split(',');
       replaced.push(...variableReplacedValues.filter(v => v.trim() !== ''));
@@ -87,7 +90,6 @@ export function replaceVariables(values: string[], templateSrv: TemplateSrv) {
 export function isSystemLinkError(error: any): error is SystemLinkError {
   return Boolean(error?.error?.code) && Boolean(error?.error?.name);
 }
-
 
 /**
  * Used for filtering XSS in query builder fields
@@ -104,12 +106,12 @@ export function filterXSSLINQExpression(value: string | null | undefined): strin
   const sanitizedTarget = textUtil.sanitize(unsanitizedTarget);
 
   return sanitizedTarget
-    .replace(/ &lt; /g, " < ")
-    .replace(/ &lt;= /g, " <= ")
-    .replace(/ &gt; /g, " > ")
-    .replace(/ &gt;= /g, " >= ")
-    .replace(/ &amp;&amp; /g, " && ")
-    .replace(/ &lt;&gt; /g, " <> ");
+    .replace(/ &lt; /g, ' < ')
+    .replace(/ &lt;= /g, ' <= ')
+    .replace(/ &gt; /g, ' > ')
+    .replace(/ &gt;= /g, ' >= ')
+    .replace(/ &amp;&amp; /g, ' && ')
+    .replace(/ &lt;&gt; /g, ' <> ');
 }
 
 export function validateNumericInput(event: React.KeyboardEvent<HTMLInputElement>) {
@@ -121,7 +123,7 @@ export function validateNumericInput(event: React.KeyboardEvent<HTMLInputElement
 export async function queryInBatches<T>(
   queryRecord: (take: number, continuationToken?: string) => Promise<QueryResponse<T>>,
   queryConfig: BatchQueryConfig,
-  take?: number,
+  take?: number
 ): Promise<QueryResponse<T>> {
   if (take === undefined || take <= queryConfig.maxTakePerRequest) {
     return await queryRecord(take || queryConfig.maxTakePerRequest);
@@ -139,9 +141,10 @@ export async function queryInBatches<T>(
   };
 
   const queryRecordsInCurrentBatch = async (): Promise<void> => {
-    const remainingRecordsToGet = totalCount !== undefined ?
-      Math.min(take - queryResponse.length, totalCount - queryResponse.length) :
-      take - queryResponse.length;
+    const remainingRecordsToGet =
+      totalCount !== undefined
+        ? Math.min(take - queryResponse.length, totalCount - queryResponse.length)
+        : take - queryResponse.length;
 
     if (remainingRecordsToGet <= 0) {
       return;
@@ -174,6 +177,48 @@ export async function queryInBatches<T>(
     data: queryResponse,
     totalCount,
   };
+}
+
+/**
+ * The function checks if the provided value is a multi-select value. If it is, it generates
+ * a query expression that combines all values using the specified operation and a logical
+ * operator (e.g., AND/OR). If the value is not a multi-select, it generates a simple query
+ * expression for the single value.
+ */
+export function multipleValuesQuery(field: string): ExpressionTransformFunction {
+  return (value: string, operation: string, _options?: any) => {
+    const isMultiSelect = isMultiSelectValue(value);
+    const valuesArray = getMultipleValuesArray(value);
+    const logicalOperator = getLogicalOperator(operation);
+
+    return isMultiSelect
+      ? `(${valuesArray.map(val => `${field} ${operation} "${val}"`).join(` ${logicalOperator} `)})`
+      : `${field} ${operation} "${value}"`;
+  };
+}
+
+/**
+ * Creates a function that generates a query expression for a given field, 
+ * applying a specified operation and value. The value is formatted to ISO 
+ * string format if it matches the placeholder '${__now:date}'.
+ */
+export function timeFieldsQuery(field: string): ExpressionTransformFunction {
+  return (value: string, operation: string): string => {
+    const formattedValue = value === '${__now:date}' ? new Date().toISOString() : value;
+    return `${field} ${operation} "${formattedValue}"`;
+  };
+}
+
+function isMultiSelectValue(value: string): boolean {
+  return value.startsWith('{') && value.endsWith('}');
+}
+
+function getMultipleValuesArray(value: string): string[] {
+  return value.replace(/({|})/g, '').split(',');
+}
+
+function getLogicalOperator(operation: string): string {
+  return operation === QueryBuilderOperations.EQUALS.name ? '||' : '&&';
 }
 
 async function delay(timeout: number): Promise<void> {
