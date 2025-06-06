6f9c271f99296b7ebd826c8f3529828bdc455055
Manisha Sivakumar <manisha.sivakumar@emerson.com>
2025-06-06 13:21:50 +0000

    update docstring
    Signed-off-by: Manisha Sivakumar <manisha.sivakumar@emerson.com>

--- src/shared/users.utils.ts
+++ src/shared/users.utils.ts
@@ -10,7 +10,7 @@ import { QUERY_USERS_MAX_TAKE, QUERY_USERS_REQUEST_PER_SECOND } from './constant
  */
 export class UsersUtils {
   /**
-   * A cached promise that resolves to an array of users.
+   * A cached promise that resolves to an map of users.
    * This cache is used to avoid redundant user data fetches.
    */
   private static _usersCache: Promise<Map<string, User>> | null = null;
@@ -19,6 +19,10 @@ export class UsersUtils {
   /**
    * Retrieves the cached promise for the list of users.
    * If the cache is not initialized, it triggers the loading of users.
+   * 
+   * Note: This implementation corresponds to the SLE users service implementation.
+   * Ensure that any changes made here are kept in sync with the SLE service implementation:
+   * [Link to SLE service implementation] (https://dev.azure.com/ni/DevCentral/_git/Skyline?path=/Web/Workspaces/SystemLinkShared/projects/systemlink-lib-angular/src/services/sl-users-service.ts&version=GBusers/jmeyer/grafana-app-update&_a=contents)
    */
   public async getUsers(): Promise<Map<string, User>> {
     if (!UsersUtils._usersCache) {
@@ -49,7 +53,7 @@ export class UsersUtils {
 
   /**
    * Loads the list of users, utilizing a cache to avoid redundant queries.
-   * In case of an error during the query, an empty array is returned, and the cache is cleared.
+   * In case of an error during the query, an empty map is returned, and the cache is cleared.
    */
   private async loadUsers(): Promise<Map<string, User>> {
     try {
