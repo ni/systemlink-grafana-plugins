fc301fef4c01d698981fa4b2d60efd770cd349db
priya-ni <priyadarshini.gopal@emerson.com>
2025-06-06 10:53:34 +0000

    address comments

--- src/shared/workspace.utils.ts
+++ src/shared/workspace.utils.ts
@@ -3,38 +3,33 @@ import { BackendSrv } from "@grafana/runtime";
 import { Workspace } from "core/types";
 
 export class WorkspaceUtils {
-    private _workspacesCache: Promise<Map<string, Workspace>> | null = null;
+    private static _workspacesCache?: Promise<Map<string, Workspace>>;
 
     private readonly queryWorkspacesUrl = `${this.instanceSettings.url}/niauth/v1/auth`;
 
     constructor(
         readonly instanceSettings: DataSourceInstanceSettings,
         readonly backendSrv: BackendSrv
-    ) {
-        this.loadWorkspaces();
-    }
+    ) {}
 
     async getWorkspaces(): Promise<Map<string, Workspace>> {
-        return this._workspacesCache ?? await this.loadWorkspaces();
+        if (!WorkspaceUtils._workspacesCache) {
+            WorkspaceUtils._workspacesCache = this.loadWorkspaces();
+        }
+        return WorkspaceUtils._workspacesCache;
     }
 
     private async loadWorkspaces(): Promise<Map<string, Workspace>> {
-        if (this._workspacesCache) {
-            return this._workspacesCache;
-        }
         try {
             const workspaces = await this.fetchWorkspaces();
             const workspaceMap = new Map<string, Workspace>();
             if (workspaces) {
                 workspaces.forEach(workspace => workspaceMap.set(workspace.id, workspace));
             }
-            this._workspacesCache = Promise.resolve(workspaceMap);
             return workspaceMap;
         } catch (error) {
             console.error('Error in loading workspaces:', error);
-            const emptyMap = new Map<string, Workspace>();
-            this._workspacesCache = Promise.resolve(emptyMap);
-            return emptyMap;
+            return new Map<string, Workspace>();
         }
     }
 
