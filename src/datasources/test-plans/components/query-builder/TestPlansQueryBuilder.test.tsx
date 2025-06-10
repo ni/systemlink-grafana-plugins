cdf4d95c546eb0cff16b274382f61665c9993893
priya-ni <priyadarshini.gopal@emerson.com>
2025-06-10 08:11:38 +0000

    add state property

--- src/datasources/test-plans/components/query-builder/TestPlansQueryBuilder.test.tsx
+++ src/datasources/test-plans/components/query-builder/TestPlansQueryBuilder.test.tsx
@@ -98,6 +98,31 @@ describe('TestPlansQueryBuilder', () => {
         expect(conditionsContainer.item(0)?.textContent).toContain("User 2");
     });
 
+    it('should select state option', () => {
+        const { conditionsContainer } = renderElement('state = "PendingApproval"', [], [], []);
+        expect(conditionsContainer?.length).toBe(1);
+        expect(conditionsContainer.item(0)?.textContent).toContain('State');
+        expect(conditionsContainer.item(0)?.textContent).toContain('Equals');
+        expect(conditionsContainer.item(0)?.textContent).toContain('Pending approval');
+    });
+    it('should select global variable option', () => {
+        const globalVariableOption = { label: 'Global variable', value: '$global_variable' };
+        const { conditionsContainer } = renderElement('state = \"$global_variable\"', [], [], [], [globalVariableOption]);
+
+        expect(conditionsContainer?.length).toBe(1);
+        expect(conditionsContainer.item(0)?.textContent).toContain(globalVariableOption.label);
+    });
+
+    it('should support key value operations', () => {
+        const { conditionsContainer } = renderElement("properties[\"key\"] = \"value\"", [], [], []);
+
+        expect(conditionsContainer?.length).toBe(1);
+        expect(conditionsContainer.item(0)?.textContent).toContain('Properties');
+        expect(conditionsContainer.item(0)?.textContent).toContain('matches');
+        expect(conditionsContainer.item(0)?.textContent).toContain('key');
+        expect(conditionsContainer.item(0)?.textContent).toContain('value');
+    });
+
     [['${__from:date}', 'From'], ['${__to:date}', 'To'], ['${__now:date}', 'Now']].forEach(([value, label]) => {
         it(`should select user friendly value for updated date`, () => {
             const { conditionsContainer } = renderElement(`updatedAt > \"${value}\"`, [], [], []);
