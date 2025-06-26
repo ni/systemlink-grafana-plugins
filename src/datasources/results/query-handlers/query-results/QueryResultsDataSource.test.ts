e4b4e8bb889a773aaf37b5877a950b7ba0dc4d6f
Ahalya-ni <ahalya.radhakrishnan@ni.com>
2025-06-26 11:17:20 +0530

    test(results,workorders,testplans): Update 429 error handling tests (#338)

--- src/datasources/results/query-handlers/query-results/QueryResultsDataSource.test.ts
+++ src/datasources/results/query-handlers/query-results/QueryResultsDataSource.test.ts
@@ -106,14 +106,14 @@ describe('QueryResultsDataSource', () => {
       });
 
     test('should throw error when API returns 429 status', async () => {
-        backendServer.fetch
-          .calledWith(requestMatching({ url: '/nitestmonitor/v2/query-results' }))
-          .mockReturnValue(createFetchError(429));
-    
-        await expect(datastore.queryResults())
-          .rejects
-          .toThrow('The query to fetch results failed due to too many requests. Please try again later.');
+      jest.spyOn(datastore, 'post').mockImplementation(() => {
+        throw new Error('Request failed with status code: 429');
       });
+
+      await expect(datastore.queryResults()).rejects.toThrow(
+        'The query to fetch results failed due to too many requests. Please try again later.'
+      );
+    });
 
     test('should throw timeOut error when API returns 504 status', async () => {
         backendServer.fetch
