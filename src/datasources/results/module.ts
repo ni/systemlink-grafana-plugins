b6e777d17006bffae33533a163984688965e8e9a
Ahalya Radhakrishnan <ahalya.radhakrishnan@ni.com>
2025-06-26 11:30:29 +0530

    added http config editor
    Signed-off-by: Ahalya Radhakrishnan <ahalya.radhakrishnan@ni.com>

--- src/datasources/results/module.ts
+++ src/datasources/results/module.ts
@@ -3,7 +3,9 @@ import { ResultsDataSource } from './ResultsDataSource';
 import { ResultsQueryEditor } from './components/ResultsQueryEditor';
 import { ResultsQuery } from './types/types';
 import { ResultsVariableQueryEditor } from './components/variable-editors/ResultsVariableQueryEditor';
+import { HttpConfigEditor } from 'core/components/HttpConfigEditor';
 
 export const plugin = new DataSourcePlugin<ResultsDataSource, ResultsQuery>(ResultsDataSource)
+  .setConfigEditor(HttpConfigEditor)
   .setQueryEditor(ResultsQueryEditor)
   .setVariableQueryEditor(ResultsVariableQueryEditor)
