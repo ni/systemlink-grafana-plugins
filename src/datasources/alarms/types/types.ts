b05659814c6dd99d4b9d1a06f6fa9b536eaa3bf1
Ahalya Radhakrishnan <ahalya.radhakrishnan@ni.com>
2025-08-29 14:51:59 +0530

    removed query type
    Signed-off-by: Ahalya Radhakrishnan <ahalya.radhakrishnan@ni.com>

--- src/datasources/alarms/types/types.ts
+++ src/datasources/alarms/types/types.ts
@@ -1,9 +1,3 @@
 import { DataQuery } from '@grafana/schema';
 
-export interface AlarmsQuery extends DataQuery {
-  queryType?: QueryType;
-};
-
-export enum QueryType {
-  AlarmsCount = 'AlarmsCount'
-};
+export interface AlarmsQuery extends DataQuery {}
