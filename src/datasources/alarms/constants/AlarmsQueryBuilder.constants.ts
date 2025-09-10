345f92ad4ca5e6a2a6f0a66d7b453d8246eff87c
Manisha Sivakumar <manisha.sivakumar@emerson.com>
2025-09-10 13:56:49 +0000

    address comments
    Signed-off-by: Manisha Sivakumar <manisha.sivakumar@emerson.com>

--- src/datasources/alarms/constants/AlarmsQueryBuilder.constants.ts
+++ src/datasources/alarms/constants/AlarmsQueryBuilder.constants.ts
@@ -1,21 +1,25 @@
 import { QueryBuilderOperations } from 'core/query-builder.constants';
 import { QBField } from 'core/types';
 
-export enum AlarmsQueryBuilderFieldNames {
-  Acknowledged = 'acknowledged',
-  Active = 'active',
-  AlarmID = 'alarmId',
-  AlarmName = 'displayName',
-  Channel = 'channel',
-  Clear = 'clear',
-  CreatedBy = 'createdBy',
-  CurrentSeverity = 'currentSeverityLevel',
-  Description = 'description',
-  HighestSeverity = 'highestSeverityLevel',
-  ResourceType = 'resourceType',
-}
+const BOOLEAN_FILTER_OPERATIONS = [
+  QueryBuilderOperations.EQUALS.name, 
+  QueryBuilderOperations.DOES_NOT_EQUAL.name
+];
 
-const SEVERITY_LEVELS = [
+const BASIC_STRING_FILTER_OPERATIONS = [
+  QueryBuilderOperations.EQUALS.name,
+  QueryBuilderOperations.DOES_NOT_EQUAL.name,
+  QueryBuilderOperations.CONTAINS.name,
+  QueryBuilderOperations.DOES_NOT_CONTAIN.name,
+];
+
+const EXTENDED_STRING_FILTER_OPERATIONS = [
+  ...BASIC_STRING_FILTER_OPERATIONS,
+  QueryBuilderOperations.IS_BLANK.name,
+  QueryBuilderOperations.IS_NOT_BLANK.name,
+];
+
+export const SEVERITY_LEVELS = [
   { label: 'Low', value: '1' },
   { label: 'Moderate', value: '2' },
   { label: 'High', value: '3' },
@@ -23,123 +27,81 @@ const SEVERITY_LEVELS = [
   { label: 'Clear', value: '-1' },
 ];
 
+export const BOOLEAN_OPTIONS = [
+  { label: 'True', value: 'true' },
+  { label: 'False', value: 'false' },
+];
+
 export const AlarmsQueryBuilderFields: Record<string, QBField> = {
   ACKNOWLEDGED: {
     label: 'Acknowledged',
-    dataField: AlarmsQueryBuilderFieldNames.Acknowledged,
-    filterOperations: [QueryBuilderOperations.EQUALS.name, QueryBuilderOperations.DOES_NOT_EQUAL.name],
+    dataField: 'acknowledged',
+    filterOperations: BOOLEAN_FILTER_OPERATIONS,
     lookup: {
-      dataSource: [
-        { label: 'True', value: 'true' },
-        { label: 'False', value: 'false' },
-      ],
+      dataSource: BOOLEAN_OPTIONS,
     },
   },
   ACTIVE: {
     label: 'Active',
-    dataField: AlarmsQueryBuilderFieldNames.Active,
-    filterOperations: [QueryBuilderOperations.EQUALS.name, QueryBuilderOperations.DOES_NOT_EQUAL.name],
+    dataField: 'active',
+    filterOperations: BOOLEAN_FILTER_OPERATIONS,
     lookup: {
-      dataSource: [
-        { label: 'True', value: 'true' },
-        { label: 'False', value: 'false' },
-      ],
+      dataSource: BOOLEAN_OPTIONS,
     },
   },
   ALARM_ID: {
     label: 'Alarm ID',
-    dataField: AlarmsQueryBuilderFieldNames.AlarmID,
-    filterOperations: [
-      QueryBuilderOperations.EQUALS.name,
-      QueryBuilderOperations.DOES_NOT_EQUAL.name,
-      QueryBuilderOperations.CONTAINS.name,
-      QueryBuilderOperations.DOES_NOT_CONTAIN.name,
-    ],
+    dataField: 'alarmId',
+    filterOperations: BASIC_STRING_FILTER_OPERATIONS,
   },
   ALARM_NAME: {
     label: 'Alarm name',
-    dataField: AlarmsQueryBuilderFieldNames.AlarmName,
-    filterOperations: [
-      QueryBuilderOperations.EQUALS.name,
-      QueryBuilderOperations.DOES_NOT_EQUAL.name,
-      QueryBuilderOperations.CONTAINS.name,
-      QueryBuilderOperations.DOES_NOT_CONTAIN.name,
-      QueryBuilderOperations.IS_BLANK.name,
-      QueryBuilderOperations.IS_NOT_BLANK.name,
-    ],
+    dataField: 'displayName',
+    filterOperations: EXTENDED_STRING_FILTER_OPERATIONS,
   },
   CHANNEL: {
     label: 'Channel',
-    dataField: AlarmsQueryBuilderFieldNames.Channel,
-    filterOperations: [
-      QueryBuilderOperations.EQUALS.name,
-      QueryBuilderOperations.DOES_NOT_EQUAL.name,
-      QueryBuilderOperations.CONTAINS.name,
-      QueryBuilderOperations.DOES_NOT_CONTAIN.name,
-      QueryBuilderOperations.IS_BLANK.name,
-      QueryBuilderOperations.IS_NOT_BLANK.name,
-    ],
+    dataField: 'channel',
+    filterOperations: EXTENDED_STRING_FILTER_OPERATIONS,
   },
   CLEAR: {
     label: 'Clear',
-    dataField: AlarmsQueryBuilderFieldNames.Clear,
-    filterOperations: [QueryBuilderOperations.EQUALS.name, QueryBuilderOperations.DOES_NOT_EQUAL.name],
+    dataField: 'clear',
+    filterOperations: BOOLEAN_FILTER_OPERATIONS,
     lookup: {
-      dataSource: [
-        { label: 'True', value: 'true' },
-        { label: 'False', value: 'false' },
-      ],
+      dataSource: BOOLEAN_OPTIONS,
     },
   },
   CREATED_BY: {
     label: 'Created by',
-    dataField: AlarmsQueryBuilderFieldNames.CreatedBy,
-    filterOperations: [
-      QueryBuilderOperations.EQUALS.name,
-      QueryBuilderOperations.DOES_NOT_EQUAL.name,
-      QueryBuilderOperations.CONTAINS.name,
-      QueryBuilderOperations.DOES_NOT_CONTAIN.name,
-      QueryBuilderOperations.IS_BLANK.name,
-      QueryBuilderOperations.IS_NOT_BLANK.name,
-    ],
+    dataField: 'createdBy',
+    filterOperations: EXTENDED_STRING_FILTER_OPERATIONS,
   },
   CURRENT_SEVERITY: {
     label: 'Current severity',
-    dataField: AlarmsQueryBuilderFieldNames.CurrentSeverity,
-    filterOperations: [QueryBuilderOperations.EQUALS.name, QueryBuilderOperations.DOES_NOT_EQUAL.name],
+    dataField: 'currentSeverityLevel',
+    filterOperations: BOOLEAN_FILTER_OPERATIONS,
     lookup: {
         dataSource: SEVERITY_LEVELS,
     },
   },
   DESCRIPTION: {
     label: 'Description',
-    dataField: AlarmsQueryBuilderFieldNames.Description,
-    filterOperations: [
-      QueryBuilderOperations.EQUALS.name,
-      QueryBuilderOperations.DOES_NOT_EQUAL.name,
-      QueryBuilderOperations.CONTAINS.name,
-      QueryBuilderOperations.DOES_NOT_CONTAIN.name,
-      QueryBuilderOperations.IS_BLANK.name,
-      QueryBuilderOperations.IS_NOT_BLANK.name,
-    ],
+    dataField: 'description',
+    filterOperations: EXTENDED_STRING_FILTER_OPERATIONS,
   },
   HIGHEST_SEVERITY: {
     label: 'Highest severity',
-    dataField: AlarmsQueryBuilderFieldNames.HighestSeverity,
-    filterOperations: [QueryBuilderOperations.EQUALS.name, QueryBuilderOperations.DOES_NOT_EQUAL.name],
+    dataField: 'highestSeverityLevel',
+    filterOperations: BOOLEAN_FILTER_OPERATIONS,
     lookup: {
       dataSource: SEVERITY_LEVELS,
     },
   },
   RESOURCE_TYPE: {
     label: 'Resource type',
-    dataField: AlarmsQueryBuilderFieldNames.ResourceType,
-    filterOperations: [
-      QueryBuilderOperations.EQUALS.name,
-      QueryBuilderOperations.DOES_NOT_EQUAL.name,
-      QueryBuilderOperations.CONTAINS.name,
-      QueryBuilderOperations.DOES_NOT_CONTAIN.name,
-    ],
+    dataField: 'resourceType',
+    filterOperations: BASIC_STRING_FILTER_OPERATIONS,
   },
 };
 
