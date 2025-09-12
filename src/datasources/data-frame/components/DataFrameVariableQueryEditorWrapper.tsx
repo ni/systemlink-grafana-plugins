325d4a201e8df020f5b1d19836bc58d3b7b59dea
Gokuldharan <gokuldharan.selvaraju@emerson.com>
2025-09-12 08:15:13 +0000

    split variable query editor

---
+++ src/datasources/data-frame/components/DataFrameVariableQueryEditorWrapper.tsx
@@ -0,0 +1,18 @@
+import React, { useRef } from 'react';
+import { Props } from './DataFrameQueryEditorCommon';
+import { DataFrameFeatureToggles, DataFrameFeatureTogglesDefaults } from '../types';
+import { DataFrameVariableQueryEditorV1 } from './v1/DataFrameVariableQueryEditorV1';
+import { DataFrameVariableQueryEditorV2 } from './v2/DataFrameVariableQueryEditorV2';
+
+export function DataFrameVariableQueryEditorWrapper(props: Props) {
+  const dataFrameFeatures = useRef<DataFrameFeatureToggles>({
+    queryByDataTableProperties: props.datasource.instanceSettings.jsonData?.featureToggles?.queryByDataTableProperties
+      ?? DataFrameFeatureTogglesDefaults.queryByDataTableProperties
+  });
+
+  return (
+    dataFrameFeatures.current.queryByDataTableProperties
+      ? <DataFrameVariableQueryEditorV2 />
+      : <DataFrameVariableQueryEditorV1 {...props} />
+  )
+}
---
+++ src/datasources/data-frame/components/v1/DataFrameVariableQueryEditorV1.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { AsyncSelect } from '@grafana/ui';
+import { InlineField } from 'core/components/InlineField';
+import { toOption } from '@grafana/data';
+import { DataFrameQueryEditorCommon, Props } from '../DataFrameQueryEditorCommon';
+import { FloatingError, parseErrorMessage } from 'core/errors';
+import { isValidId } from 'datasources/data-frame/utils';
+
+export function DataFrameVariableQueryEditorV1(props: Props) {
+    const [errorMsg, setErrorMsg] = useState<string | undefined>('');
+    const handleError = (error: Error) => setErrorMsg(parseErrorMessage(error));
+    const common = new DataFrameQueryEditorCommon(props, handleError);
+
+    return (
+        <div style={{ position: 'relative' }}>
+            <InlineField label="Id">
+                <AsyncSelect
+                    allowCreateWhileLoading
+                    allowCustomValue
+                    cacheOptions={false}
+                    defaultOptions
+                    isValidNewOption={isValidId}
+                    loadOptions={common.handleLoadOptions}
+                    onChange={common.handleIdChange}
+                    placeholder="Search by name or enter id"
+                    width={30}
+                    value={common.query.tableId ? toOption(common.query.tableId) : null}
+                />
+            </InlineField>
+            <FloatingError message={errorMsg} />
+        </div>
+    );
+}
