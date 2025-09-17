48d2c65a1141b61c9dbc78c21e2d8f7a364cdbd4
Gokuldharan <gokuldharan.selvaraju@emerson.com>
2025-09-17 11:58:04 +0000

    add query type control

--- src/datasources/data-frame/components/v2/DataFrameQueryEditorV2.tsx
+++ src/datasources/data-frame/components/v2/DataFrameQueryEditorV2.tsx
@@ -1,11 +1,37 @@
+import { InlineField, RadioButtonGroup } from "@grafana/ui";
+import { enumToOptions } from "core/utils";
+import { DataFrameQuery, DataFrameQueryType } from "datasources/data-frame/types";
 import React from "react";
+import { Props } from "../DataFrameQueryEditorCommon";
+import { CoreApp } from "@grafana/data";
 
-export const DataFrameQueryEditorV2 = () => {
+export const DataFrameQueryEditorV2 = (props: Props) => {
     // TODO AB#3259790: Add `Data table properties` Query Builder
 
+    const onRunQuery = () => {
+        if (props.app !== CoreApp.Explore) {
+            props.onRunQuery();
+        }
+    }
+    const onQueryTypeChange = (value: DataFrameQuery, runQuery: boolean) => {
+        props.onChange(value);
+        if (runQuery) {
+            onRunQuery();
+        }
+    };
+    const query = props.datasource.processQuery(props.query);
+
     return (
-        <div>
-            <p>DataFrame Query Editor V2 is under development.</p>
-        </div>
+        <InlineField label="Query type" tooltip={tooltips.queryType}>
+            <RadioButtonGroup
+                options={enumToOptions(DataFrameQueryType)}
+                value={query.type}
+                onChange={value => onQueryTypeChange({ ...query, type: value }, true)}
+            />
+        </InlineField>
     );
+}
+
+const tooltips = {
+    queryType: `Specifies whether to visualize the data rows or properties associated with a table.`,
 }
