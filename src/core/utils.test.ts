c8fd295d8c739d53578812b0587cc83e498f0560
Ahalya Radhakrishnan <ahalya.radhakrishnan@ni.com>
2025-10-10 18:11:16 +0530

    remove unused imports
    Signed-off-by: Ahalya Radhakrishnan <ahalya.radhakrishnan@ni.com>

--- src/core/utils.test.ts
+++ src/core/utils.test.ts
@@ -2,7 +2,6 @@ import { BackendSrv, TemplateSrv } from "@grafana/runtime";
 import { validateNumericInput, enumToOptions, filterXSSField, filterXSSLINQExpression, replaceVariables, queryInBatches, queryUsingSkip, queryUntilComplete, getVariableOptions, get, post, addOptionsToLookup, multipleValuesQuery, timeFieldsQuery, buildExpression, isMultiValueExpression, getLogicalOperator, getMultipleValuesArray } from "./utils";
 import { BatchQueryConfig, QBField, QueryBuilderOption } from "./types";
 import { of, throwError } from 'rxjs';
-import { forEach } from "lodash";
 
 const mockBackendSrv = {
   fetch: jest.fn(),
