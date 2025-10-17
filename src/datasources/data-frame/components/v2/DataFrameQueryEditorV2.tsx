dd20a0a4e87b4213fe0cfd593431ec52af29fb6d
Richie Soshan <130976474+richie-ni@users.noreply.github.com>
2025-10-17 17:31:07 +0530

    feat(dataframe): Introduce top level DataFrameDataSource (#427)

--- src/datasources/data-frame/components/v2/DataFrameQueryEditorV2.tsx
+++ src/datasources/data-frame/components/v2/DataFrameQueryEditorV2.tsx
@@ -1,18 +1,18 @@
 import React, { useCallback, useState } from 'react';
 import { DataTableQueryBuilder } from "./query-builders/DataTableQueryBuilder";
 import { AutoSizeInput, Collapse, InlineField, InlineLabel, MultiSelect, RadioButtonGroup } from "@grafana/ui";
-import { DataFrameQueryV1, DataFrameQueryType, PropsV1 } from "../../types";
+import { DataFrameQueryV2, DataFrameQueryType, PropsV2 } from "../../types";
 import { enumToOptions, validateNumericInput } from "core/utils";
 import { TAKE_LIMIT } from 'datasources/data-frame/constants';
 
-export const DataFrameQueryEditorV2: React.FC<PropsV1> = ({ query, onChange, onRunQuery, datasource }: PropsV1) => {
+export const DataFrameQueryEditorV2: React.FC<PropsV2> = ({ query, onChange, onRunQuery, datasource }: PropsV2) => {
     query = datasource.processQuery(query);
 
     const [isQueryConfigurationSectionOpen, setIsQueryConfigurationSectionOpen] = useState(true);
     const [recordCountInvalidMessage, setRecordCountInvalidMessage] = useState<string>('');
 
     const handleQueryChange = useCallback(
-        (query: DataFrameQueryV1, runQuery = true): void => {
+        (query: DataFrameQueryV2, runQuery = true): void => {
             onChange(query);
             if (runQuery) {
                 onRunQuery();
--- src/datasources/data-frame/types.ts
+++ src/datasources/data-frame/types.ts
@@ -2,6 +2,7 @@ import { DataQuery } from '@grafana/schema';
 import { SystemLinkError } from "../../core/types";
 import { DataSourceJsonData, QueryEditorProps } from '@grafana/data';
 import { DataFrameDataSourceV1 } from './datasources/v1/DataFrameDataSourceV1';
+import { DataFrameDataSourceV2 } from './datasources/v2/DataFrameDataSourceV2';
 
 export enum DataFrameQueryType {
   Data = 'Data',
@@ -48,7 +49,11 @@ export type ValidDataFrameQueryV2 = DataFrameQueryV2 & Required<Omit<DataFrameQu
 
 export type ColumnDataType = 'BOOL' | 'INT32' | 'INT64' | 'FLOAT32' | 'FLOAT64' | 'STRING' | 'TIMESTAMP';
 
+export type Props = PropsV1 | PropsV2;
+
 export type PropsV1 = QueryEditorProps<DataFrameDataSourceV1, DataFrameQueryV1, DataFrameDataSourceOptions>;
+
+export type PropsV2 = QueryEditorProps<DataFrameDataSourceV2, DataFrameQueryV2, DataFrameDataSourceOptions>;
 
 export interface Column {
   name: string;
