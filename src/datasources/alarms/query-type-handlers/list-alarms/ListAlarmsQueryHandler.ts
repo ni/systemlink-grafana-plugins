ca264601cf53d3bc042aa9bf11231aa0244a43be
Ahalya-ni <ahalya.radhakrishnan@ni.com>
2025-10-28 19:31:36 +0530

    feat(alarms): Add support for users with error handling (#465)

--- src/datasources/alarms/query-type-handlers/list-alarms/ListAlarmsQueryHandler.ts
+++ src/datasources/alarms/query-type-handlers/list-alarms/ListAlarmsQueryHandler.ts
@@ -1,11 +1,25 @@
-import { DataFrameDTO, DataQueryRequest, LegacyMetricFindQueryOptions, MetricFindValue } from '@grafana/data';
+import { DataFrameDTO, DataQueryRequest, DataSourceInstanceSettings, LegacyMetricFindQueryOptions, MetricFindValue } from '@grafana/data';
 import { ListAlarmsQuery } from '../../types/ListAlarms.types';
 import { AlarmsVariableQuery } from '../../types/types';
 import { AlarmsQueryHandlerCore } from '../AlarmsQueryHandlerCore';
 import { defaultListAlarmsQuery } from 'datasources/alarms/constants/DefaultQueries.constants';
+import { BackendSrv, getBackendSrv, getTemplateSrv, TemplateSrv } from '@grafana/runtime';
+import { User } from 'shared/types/QueryUsers.types';
+import { UsersUtils } from 'shared/users.utils';
 
 export class ListAlarmsQueryHandler extends AlarmsQueryHandlerCore {
   public readonly defaultQuery = defaultListAlarmsQuery;
+
+  private readonly usersUtils: UsersUtils;
+
+  public constructor(
+    instanceSettings: DataSourceInstanceSettings,
+    backendSrv: BackendSrv = getBackendSrv(),
+    templateSrv: TemplateSrv = getTemplateSrv()
+  ) {
+    super(instanceSettings, backendSrv, templateSrv);
+    this.usersUtils = new UsersUtils(this.instanceSettings, this.backendSrv);
+  }
 
   public async runQuery(query: ListAlarmsQuery, _options: DataQueryRequest): Promise<DataFrameDTO> {
     return {
@@ -36,6 +50,19 @@ export class ListAlarmsQueryHandler extends AlarmsQueryHandlerCore {
       return sortedOptions;
     } catch (error) {
       return [];
+    }
+  }
+
+  // @ts-ignore
+  // #AB:3249477 Suppress unused method warning until properties control support is implemented
+  private async loadUsers(): Promise<Map<string, User>> {
+    try {
+      return await this.usersUtils.getUsers();
+    } catch (error) {
+      if (!this.errorTitle) {
+        this.handleDependenciesError(error);
+      }
+      return new Map<string, User>();
     }
   }
 }
