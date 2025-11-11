1cb7c8d4217c5007984b66ecf2faeac257124b6d
richie.soshan@ni.com <richie.soshan@ni.com>
2025-11-11 13:45:55 +0530

    test(datasources): add test for getColumnOptions returning an empty array

--- src/datasources/data-frame/DataFrameDataSourceBase.test.ts
+++ src/datasources/data-frame/DataFrameDataSourceBase.test.ts
@@ -126,7 +126,9 @@ describe('DataFrameDataSourceBase', () => {
 
     it('should return empty array for getColumnOptions', async () => {
         const ds = new TestDataFrameDataSource(instanceSettings, backendSrv, templateSrv);
+        
         const options = await ds.getColumnOptions('filter');   
+        
         expect(options).toEqual([]);
     });
 
