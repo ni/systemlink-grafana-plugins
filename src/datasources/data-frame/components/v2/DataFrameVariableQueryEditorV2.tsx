05d83db3808fa40aa7a92f0206a743a6b4cd092b
Gokuldharan <gokuldharan.selvaraju@emerson.com>
2025-11-12 08:29:30 +0000

    integrate query builder wrapper

--- src/datasources/data-frame/components/v2/DataFrameVariableQueryEditorV2.tsx
+++ src/datasources/data-frame/components/v2/DataFrameVariableQueryEditorV2.tsx
@@ -4,6 +4,7 @@ import { FloatingError } from "core/errors";
 import { INLINE_LABEL_WIDTH } from "datasources/data-frame/constants/v2/DataFrameQueryEditorV2.constants";
 import { DataFrameVariableQuery, DataFrameVariableQueryType, Props } from "datasources/data-frame/types";
 import React from "react";
+import { DataFrameQueryBuilderWrapper } from "./query-builders/DataFrameQueryBuilderWrapper";
 
 export const DataFrameVariableQueryEditorV2: React.FC<Props> = ({ query, onChange, datasource }: Props) => {
     const migratedQuery = datasource.processVariableQuery(query);
@@ -14,6 +15,13 @@ export const DataFrameVariableQueryEditorV2: React.FC<Props> = ({ query, onChang
 
     const onQueryTypeChange = (option: ComboboxOption<string>) => {
         onChange({ ...migratedQuery, queryType: option.value } as DataFrameVariableQuery);
+    };
+
+    const onDataTableFilterChange = (event?: Event | React.FormEvent<Element>) => {
+        if (event) {
+            const dataTableFilter = (event as CustomEvent).detail.linq;
+            onChange({ ...migratedQuery, dataTableFilter } as DataFrameVariableQuery);
+        }
     };
 
     return (
@@ -30,7 +38,11 @@ export const DataFrameVariableQueryEditorV2: React.FC<Props> = ({ query, onChang
                     placeholder={placeholder}
                     width={40} />
             </InlineField>
-            {/* TODO: #3463943 - Integrate query builder wrapper to variable query editor */}
+            <DataFrameQueryBuilderWrapper
+                datasource={datasource}
+                dataTableFilter={migratedQuery.dataTableFilter}
+                onDataTableFilterChange={onDataTableFilterChange}
+            />
             <FloatingError
                 message={datasource.errorTitle}
                 innerMessage={datasource.errorDescription}
