2a8b32a87f52736d274df715a9a15f812ada5dd4
Ahalya Radhakrishnan <ahalya.radhakrishnan@ni.com>
2025-11-14 16:33:54 +0530

    initial commit
    Signed-off-by: Ahalya Radhakrishnan <ahalya.radhakrishnan@ni.com>

--- src/datasources/alarms/components/editors/list-alarms/ListAlarmsQueryEditor.tsx
+++ src/datasources/alarms/components/editors/list-alarms/ListAlarmsQueryEditor.tsx
@@ -19,10 +19,10 @@ import {
 } from 'datasources/alarms/constants/AlarmsQueryEditor.constants';
 import { Workspace } from 'core/types';
 import { FloatingError } from 'core/errors';
-import { AlarmsProperties, ListAlarmsQuery } from 'datasources/alarms/types/ListAlarms.types';
+import { AlarmsProperties, ListAlarmsQuery, OutputType } from 'datasources/alarms/types/ListAlarms.types';
 import { ListAlarmsQueryHandler } from 'datasources/alarms/query-type-handlers/list-alarms/ListAlarmsQueryHandler';
-import { AutoSizeInput, Combobox, ComboboxOption, InlineSwitch, MultiCombobox, Stack } from '@grafana/ui';
-import { validateNumericInput } from 'core/utils';
+import { AutoSizeInput, Combobox, ComboboxOption, InlineSwitch, MultiCombobox, RadioButtonGroup, Stack } from '@grafana/ui';
+import { enumToOptions, validateNumericInput } from 'core/utils';
 import { TransitionInclusionOption } from 'datasources/alarms/types/types';
 
 type Props = {
@@ -90,25 +90,42 @@ export function ListAlarmsQueryEditor({ query, handleQueryChange, datasource }: 
     handleQueryChange({ ...query, transitionInclusionOption: option.value });
   };
 
+  const onOutputTypeChange = (value: OutputType) => {
+    handleQueryChange({ ...query, outputType: value });
+  };
+
   return (
     <Stack direction='column'>
       <InlineField
-        label={labels.properties}
+        label={labels.outputType}
         labelWidth={LABEL_WIDTH}
-        tooltip={tooltips.properties}
-        invalid={!isPropertiesControlValid}
-        error={PROPERTIES_ERROR_MESSAGE}
+        tooltip={tooltips.outputType}
       >
-        <MultiCombobox
-          placeholder={placeholders.properties}
-          options={Object.values(AlarmsPropertiesOptions)}
-          onChange={onPropertiesChange}
-          value={query.properties}
-          width='auto'
-          minWidth={CONTROL_WIDTH}
-          maxWidth={CONTROL_WIDTH}
+        <RadioButtonGroup
+          options={enumToOptions(OutputType)}
+          value={query.outputType}
+          onChange={onOutputTypeChange}
         />
       </InlineField>
+      {query.outputType === OutputType.Properties && (
+        <InlineField
+          label={labels.properties}
+          labelWidth={LABEL_WIDTH}
+          tooltip={tooltips.properties}
+          invalid={!isPropertiesControlValid}
+          error={PROPERTIES_ERROR_MESSAGE}
+        >
+          <MultiCombobox
+            placeholder={placeholders.properties}
+            options={Object.values(AlarmsPropertiesOptions)}
+            onChange={onPropertiesChange}
+            value={query.properties}
+            width='auto'
+            minWidth={CONTROL_WIDTH}
+            maxWidth={CONTROL_WIDTH}
+          />
+        </InlineField>
+      )}
       <Stack>
         <InlineField
           label={labels.queryBy}
@@ -122,49 +139,51 @@ export function ListAlarmsQueryEditor({ query, handleQueryChange, datasource }: 
             onChange={onFilterChange}
           />
         </InlineField>
-        <Stack direction='column'>
-          <InlineField
-            label={labels.transitionInclusion}
-            labelWidth={SECONDARY_LABEL_WIDTH}
-            tooltip={tooltips.transitionInclusion}
-          >
-            <Combobox
-              options={Object.values(AlarmsTransitionInclusionOptions)}
-              value={query.transitionInclusionOption}
-              width={SECONDARY_CONTROL_WIDTH}
-              onChange={onTransitionInclusionChange}
-            />
-          </InlineField>
-          <InlineField
-            label={labels.descending}
-            labelWidth={SECONDARY_LABEL_WIDTH}
-            tooltip={tooltips.descending}
-          >
-            <InlineSwitch
-              onChange={event => onDescendingChange(event.currentTarget.checked)}
-              value={query.descending}
-            />
-          </InlineField>
-          <InlineField
-            label={labels.take}
-            labelWidth={SECONDARY_LABEL_WIDTH}
-            tooltip={tooltips.take}
-            invalid={!!takeInvalidMessage}
-            error={takeInvalidMessage}
-          >
-            <AutoSizeInput
-              minWidth={SECONDARY_CONTROL_WIDTH}
-              maxWidth={SECONDARY_CONTROL_WIDTH}
-              type="number"
-              value={query.take}
-              onChange={onTakeChange}
-              placeholder={placeholders.take}
-              onKeyDown={event => {
-                validateNumericInput(event);
-              }}
-            />
-          </InlineField>
-        </Stack>
+        {query.outputType === OutputType.Properties && (
+          <Stack direction='column'>
+            <InlineField
+              label={labels.transitionInclusion}
+              labelWidth={SECONDARY_LABEL_WIDTH}
+              tooltip={tooltips.transitionInclusion}
+            >
+              <Combobox
+                options={Object.values(AlarmsTransitionInclusionOptions)}
+                value={query.transitionInclusionOption}
+                width={SECONDARY_CONTROL_WIDTH}
+                onChange={onTransitionInclusionChange}
+              />
+            </InlineField>
+            <InlineField
+              label={labels.descending}
+              labelWidth={SECONDARY_LABEL_WIDTH}
+              tooltip={tooltips.descending}
+            >
+              <InlineSwitch
+                onChange={event => onDescendingChange(event.currentTarget.checked)}
+                value={query.descending}
+              />
+            </InlineField>
+            <InlineField
+              label={labels.take}
+              labelWidth={SECONDARY_LABEL_WIDTH}
+              tooltip={tooltips.take}
+              invalid={!!takeInvalidMessage}
+              error={takeInvalidMessage}
+            >
+              <AutoSizeInput
+                minWidth={SECONDARY_CONTROL_WIDTH}
+                maxWidth={SECONDARY_CONTROL_WIDTH}
+                type="number"
+                value={query.take}
+                onChange={onTakeChange}
+                placeholder={placeholders.take}
+                onKeyDown={event => {
+                  validateNumericInput(event);
+                }}
+              />
+            </InlineField>
+          </Stack>
+        )}
       </Stack>
       <FloatingError
         message={datasource.errorTitle}
