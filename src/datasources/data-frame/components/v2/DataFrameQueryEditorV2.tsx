90f75a6f816b977edbd9f54ad50b9d1b3d8112fa
Gokuldharan <gokuldharan.selvaraju@emerson.com>
2025-11-19 16:07:57 +0000

    fix type issue

--- src/datasources/data-frame/components/v2/DataFrameQueryEditorV2.tsx
+++ src/datasources/data-frame/components/v2/DataFrameQueryEditorV2.tsx
@@ -67,12 +67,12 @@ export const DataFrameQueryEditorV2: React.FC<Props> = ({ query, onChange, onRun
         setIsPropertiesNotSelected(isDataTablePropertiesEmpty && isColumnPropertiesEmpty);
     }, [migratedQuery.dataTableProperties, migratedQuery.columnProperties]);
 
-        useEffect(() => {
-            if (migratedQuery.type === DataFrameQueryType.Data) {
-                fetchAndSetColumnOptions(migratedQuery.dataTableFilter);
-            }
-            // eslint-disable-next-line react-hooks/exhaustive-deps
-        }, [migratedQuery.type, migratedQuery.dataTableFilter]);
+    useEffect(() => {
+        if (migratedQuery.type === DataFrameQueryType.Data) {
+            fetchAndSetColumnOptions(migratedQuery.dataTableFilter);
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [migratedQuery.type, migratedQuery.dataTableFilter]);
 
     const onQueryTypeChange = (queryType: DataFrameQueryType) => {
         handleQueryChange({ ...migratedQuery, type: queryType });
