a94e215d257bb5c673cc62f2a5d488b632b7aa85
Manisha Sivakumar <manisha.sivakumar@emerson.com>
2025-11-26 12:31:49 +0000

    remove unnecessary change
    Signed-off-by: Manisha Sivakumar <manisha.sivakumar@emerson.com>

--- src/datasources/data-frame/datasources/v2/DataFrameDataSourceV2.ts
+++ src/datasources/data-frame/datasources/v2/DataFrameDataSourceV2.ts
@@ -518,7 +518,7 @@ export class DataFrameDataSourceV2 extends DataFrameDataSourceBase {
         const projectionExcludingId = projections
             .filter(projection => projection !== DataTableProjections.Id);
         const tables$ = this.queryTables$(
-            { resultFilter: processedQuery.resultFilter || '', dataTableFilter: processedQuery.dataTableFilter },
+            { dataTableFilter: processedQuery.dataTableFilter },
             processedQuery.take,
             projectionExcludingId
         );
