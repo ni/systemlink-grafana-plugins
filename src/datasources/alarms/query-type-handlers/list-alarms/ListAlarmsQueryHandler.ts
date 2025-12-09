ad00464b4561879ade98de81b916ae7f458c06be
Ahalya-ni <ahalya.radhakrishnan@ni.com>
2025-12-09 10:24:36 +0530

    fix(alarms): Enhance property field typing (#570)

--- src/datasources/alarms/query-type-handlers/list-alarms/ListAlarmsQueryHandler.ts
+++ src/datasources/alarms/query-type-handlers/list-alarms/ListAlarmsQueryHandler.ts
@@ -158,10 +158,10 @@ export class ListAlarmsQueryHandler extends AlarmsQueryHandlerCore {
       const field = AlarmsPropertiesOptions[property];
       const fieldName = field.label;
       const fieldValue = field.value;
-      const fieldType = this.isTimeField(fieldValue) ? FieldType.time : FieldType.string;
+      const fieldType = this.getFieldTypeForProperty(fieldValue);
 
       const fieldValues = flattenedAlarms.map(alarm => {
-        const transition = alarm.transitions?.[0];
+        const transition = alarm.transitions[0];
 
         switch (property) {
           case AlarmsSpecificProperties.workspace:
@@ -181,15 +181,15 @@ export class ListAlarmsQueryHandler extends AlarmsQueryHandlerCore {
           case AlarmsSpecificProperties.source:
             return this.getSource(alarm.properties);
           case AlarmsTransitionProperties.transitionSeverityLevel:
-            return transition ? this.getSeverityLabel(transition.severityLevel) : '';
+            return this.getSeverityLabel(transition.severityLevel);
           case AlarmsTransitionProperties.transitionProperties:
-            return transition ? this.getSortedCustomProperties(transition.properties) : '';
+            return this.getSortedCustomProperties(transition.properties);
           default:
             let value;
 
             if (this.isAlarmTransitionProperty(property)) {
               const transitionKey = TransitionPropertyKeyMap[property];
-              value = transition?.[transitionKey];
+              value = transition[transitionKey];
             } else {
               const alarmKey = AlarmPropertyKeyMap[property];
               value = alarm[alarmKey];
@@ -208,6 +208,48 @@ export class ListAlarmsQueryHandler extends AlarmsQueryHandlerCore {
     return mappedFields;
   }
 
+  private getFieldTypeForProperty(field: AlarmsProperties): FieldType {
+    switch (true) {
+      case this.isTimeField(field):
+        return FieldType.time;
+      case this.isNumberField(field):
+        return FieldType.number;
+      case this.isBooleanField(field):
+        return FieldType.boolean;
+      case this.isObjectField(field):
+      case this.isArrayField(field):
+        return FieldType.other;
+      default:
+        return FieldType.string;
+    }
+  }
+
+  private isNumberField(field: AlarmsProperties): boolean {
+    return field === AlarmsSpecificProperties.transitionOverflowCount;
+  }
+
+  private isBooleanField(field: AlarmsProperties): boolean {
+    return (
+      field === AlarmsSpecificProperties.acknowledged ||
+      field === AlarmsSpecificProperties.active ||
+      field === AlarmsSpecificProperties.clear
+    );
+  }
+
+  private isObjectField(field: AlarmsProperties): boolean {
+    return (
+      field === AlarmsSpecificProperties.properties ||
+      field === AlarmsTransitionProperties.transitionProperties
+    );
+  }
+
+  private isArrayField(field: AlarmsProperties): boolean {
+    return (
+      field === AlarmsSpecificProperties.keywords ||
+      field === AlarmsTransitionProperties.transitionKeywords
+    );
+  }
+
   private hasTransitionProperties(properties: AlarmsProperties[]): boolean {
     return properties.some(prop => this.isAlarmTransitionProperty(prop));
   }
@@ -220,9 +262,9 @@ export class ListAlarmsQueryHandler extends AlarmsQueryHandlerCore {
     );
   }
 
-  private getSortedCustomProperties(properties: { [key: string]: string }): string {
+  private getSortedCustomProperties(properties: { [key: string]: string }): { [key: string]: string } {
     if (Object.keys(properties).length <= 0) {
-      return '';
+      return {};
     }
 
     const filteredEntries = Object.entries(properties)
@@ -231,7 +273,7 @@ export class ListAlarmsQueryHandler extends AlarmsQueryHandlerCore {
   
     const sortedProperties = Object.fromEntries(filteredEntries);
     
-    return JSON.stringify(sortedProperties);
+    return sortedProperties;
   }
 
   private getSeverityLabel(severityLevel: number): string {
