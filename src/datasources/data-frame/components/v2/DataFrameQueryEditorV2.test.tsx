bfb2f79f84713b66b7556ac6dba8ec065e338d25
Kartheeswaran Subramaniam <kartheeswaran.subramaniam@emerson.com>
2026-01-29 09:50:31 +0000

    fix(test): remove unnecessary blank line in DataFrameQueryEditorV2 test suite

--- src/datasources/data-frame/components/v2/DataFrameQueryEditorV2.test.tsx
+++ src/datasources/data-frame/components/v2/DataFrameQueryEditorV2.test.tsx
@@ -1539,7 +1539,6 @@ describe("DataFrameQueryEditorV2", () => {
                         expect(onRunQuery).toHaveBeenCalled();
                     });
                 });
-
             });
         });
 
