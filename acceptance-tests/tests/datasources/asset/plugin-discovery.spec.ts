00d6220b8d503a6087f52d4acc3386df67474235
Blanka Onodi <blanka.onodi@emerson.com>
2026-02-10 14:10:54 +0200

    update test

--- acceptance-tests/tests/datasources/asset/plugin-discovery.spec.ts
+++ acceptance-tests/tests/datasources/asset/plugin-discovery.spec.ts
@@ -4,6 +4,6 @@ import { GRAFANA_URL } from '../../../config/environment';
 test.describe('Assets Plugin Discovery', () => {
     test('should appear in datasource list', async ({ page }) => {
         await page.goto(`${GRAFANA_URL}/connections/datasources/new`);
-        await expect(page.locator('button:has-text("SystemLink Assets")')).toBeVisible();
+        await expect(page.locator('button:has-text("SystemLink Assets")')).not.toBeVisible();
     });
 });
