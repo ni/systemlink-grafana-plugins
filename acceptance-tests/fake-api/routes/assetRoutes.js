bd5946c8c31841337fb0b6eadccf8cabfa91f597
Blanka Onodi <blanka.onodi@emerson.com>
2026-02-13 13:13:04 +0200

    Addressing PR comments: changed the nested if else statements

--- acceptance-tests/fake-api/routes/assetRoutes.js
+++ acceptance-tests/fake-api/routes/assetRoutes.js
@@ -14,19 +14,31 @@ class AssetRoutes {
         const scanCodeFilterForFirstAssetFromDb = 'ScanCode = "scanCode1"';
         const scanCodeFilterForSixthAssetFromDb = 'ScanCode = "scanCode6"';
 
-        if (req.method === 'POST') {
-            if (req.body.filter === idFilterForFirstAssetFromDb) {
-                res.status(200).json({ assets: db.assets.filter(asset => asset.id === 'id1'), totalCount: 1 });
-            } else if (req.body.filter === idFilterForSecondAssetFromDb) {
-                res.status(200).json({ assets: db.assets.filter(asset => asset.id === 'id2'), totalCount: 1 });
-            } else if (req.body.filter === scanCodeFilterForFirstAssetFromDb) {
-                res.status(200).json({ assets: db.assets.filter(asset => asset.scanCode === 'scanCode1'), totalCount: 1 });
-            } else if (req.body.filter === scanCodeFilterForSixthAssetFromDb) {
-                res.status(200).json({ assets: db.assets.filter(asset => asset.scanCode === 'scanCode6'), totalCount: 1 });
-            } else {
-                res.status(200).json({ assets: db.assets, totalCount: db.assets.length });
-            }
+        if (req.method !== 'POST') {
+            return;
         }
+
+        if (req.body.filter === idFilterForFirstAssetFromDb) {
+            res.status(200).json({ assets: db.assets.filter(asset => asset.id === 'id1'), totalCount: 1 });
+            return;
+        }
+
+        if (req.body.filter === idFilterForSecondAssetFromDb) {
+            res.status(200).json({ assets: db.assets.filter(asset => asset.id === 'id2'), totalCount: 1 });
+            return;
+        }
+
+        if (req.body.filter === scanCodeFilterForFirstAssetFromDb) {
+            res.status(200).json({ assets: db.assets.filter(asset => asset.scanCode === 'scanCode1'), totalCount: 1 });
+            return;
+        }
+
+        if (req.body.filter === scanCodeFilterForSixthAssetFromDb) {
+            res.status(200).json({ assets: db.assets.filter(asset => asset.scanCode === 'scanCode6'), totalCount: 1 });
+            return;
+        }
+
+        res.status(200).json({ assets: db.assets, totalCount: db.assets.length });
     }
 }
 
