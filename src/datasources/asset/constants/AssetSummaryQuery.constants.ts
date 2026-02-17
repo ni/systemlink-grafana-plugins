ac31b03c56a91141bfe3473c0de2417ee0046de5
Blanka Onodi <blanka.onodi@emerson.com>
2026-02-17 10:24:04 +0200

    fix(asset): Add "outForCalibration" property to asset summary table (#681)

--- src/datasources/asset/constants/AssetSummaryQuery.constants.ts
+++ src/datasources/asset/constants/AssetSummaryQuery.constants.ts
@@ -3,5 +3,6 @@ export const assetSummaryFields = {
     ACTIVE: 'Active',
     NOT_ACTIVE: 'Not active',
     APPROACHING_DUE_DATE: 'Approaching due date',
-    PAST_DUE_DATE: 'Past due date'
+    PAST_DUE_DATE: 'Past due date',
+    OUT_FOR_CALIBRATION: 'Out for calibration'
 };
