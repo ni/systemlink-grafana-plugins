86d85f9c8ab3f94e711d4bfdf62ae446a85f352b
Ahalya Radhakrishnan <ahalya.radhakrishnan@ni.com>
2026-03-04 11:58:58 +0530

    update uirevision value
    Signed-off-by: Ahalya Radhakrishnan <ahalya.radhakrishnan@ni.com>

--- src/panels/plotly/PlotlyPanel.test.tsx
+++ src/panels/plotly/PlotlyPanel.test.tsx
@@ -796,6 +796,22 @@ describe('PlotlyPanel', () => {
 
         expect(xAxisRangeInPlot).toEqual([1.5, 2.5]);
       });
+
+      it('should reset zoom when x-axis field changes', () => {
+        const props = createMockProps({ xAxis: { field: 'temperature' } }, 1);
+
+        const { rerender } = renderPlotlyElement(props);
+
+        triggerRelayout(1.5, 2.5);
+
+        expect(xAxisRangeInPlot).toEqual([1.5, 2.5]);
+
+        const newProps = createMockProps({ xAxis: { field: 'pressure' } }, 1, 'number', 'pressure');
+
+        rerender(<PlotlyPanel {...newProps} />);
+
+        expect(xAxisRangeInPlot).toEqual([1, 3]);
+      });
     });
   });
 });
