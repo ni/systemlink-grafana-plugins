86953333ddb8986ebb9a5e8f405da84c415a8769
Ahalya Radhakrishnan <ahalya.radhakrishnan@ni.com>
2026-05-29 07:06:34 +0530

    remove feature flag toggle reference
    Signed-off-by: Ahalya Radhakrishnan <ahalya.radhakrishnan@ni.com>

--- src/datasources/data-frame/components/v2/DataFrameQueryEditorV2.test.tsx
+++ src/datasources/data-frame/components/v2/DataFrameQueryEditorV2.test.tsx
@@ -86,9 +86,7 @@ const renderComponent = (
         variablesCache,
         parseColumnIdentifier: mockParseColumnIdentifier,
         instanceSettings: {
-            jsonData: {
-                featureToggles: {}
-            }
+            jsonData: {}
         },
         ...mockDatasource
     } as unknown as DataFrameDataSource;
@@ -1179,9 +1177,7 @@ describe("DataFrameQueryEditorV2", () => {
                             hasRequiredFilters: mockHasRequiredFilters,
                             parseColumnIdentifier: mockParseColumnIdentifier,
                             instanceSettings: {
-                                jsonData: {
-                                    featureToggles: {}
-                                }
+                                jsonData: {}
                             }
                         } as any;
                     });
@@ -3702,9 +3698,7 @@ describe("DataFrameQueryEditorV2", () => {
                     getCustomPropertyOptions: mockGetCustomPropertyOptions,
                     parseColumnIdentifier: mockParseColumnIdentifier,
                     instanceSettings: {
-                        jsonData: {
-                            featureToggles: {}
-                        }
+                        jsonData: {}
                     }
                 } as any;
 
@@ -3776,9 +3770,7 @@ describe("DataFrameQueryEditorV2", () => {
                     getCustomPropertyOptions: mockGetCustomPropertyOptions,
                     parseColumnIdentifier: mockParseColumnIdentifier,
                     instanceSettings: {
-                        jsonData: {
-                            featureToggles: {}
-                        }
+                        jsonData: {}
                     }
                 } as any;
 
