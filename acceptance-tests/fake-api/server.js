0a0311c0f9514ec9bb1bf4df4026e8db870d8ccd
Ahalya Radhakrishnan <ahalya.radhakrishnan@ni.com>
2026-09-23 08:32:37 +0530

    revert unwanted changes in sercer.js
    Signed-off-by: Ahalya Radhakrishnan <ahalya.radhakrishnan@ni.com>

--- acceptance-tests/fake-api/server.js
+++ acceptance-tests/fake-api/server.js
@@ -37,7 +37,7 @@ server.get('/ninbexecution/v1/executions/:executionId', notebookRoutes.getExecut
 server.post('/niapp/v1/webapps/query', notebookRoutes.listNotebooks);
 server.get('/ninbparser/v1/notebook/:id', notebookRoutes.getNotebookMetadata);
 
-server.get('/niauth/v1/auth', authRoutes.getUserWorkspaces);
+server.get('/niauth/v1/auth', authRoutes.authenticate);
 server.post('/niworkitem/v1/query-workitems', workItemRoutes.queryWorkItems);
 server.get('/nitag/v2/tags-count', tagRoutes.getTagsCount);
 server.post('/nitag/v2/fetch-tags-with-values', tagRoutes.fetchTagsWithValues);
