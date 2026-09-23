cf6af2c7dfd3fd38d02a0e92a874d6176c1e96eb
Ahalya Radhakrishnan <ahalya.radhakrishnan@ni.com>
2026-09-23 16:13:27 +0530

    resolve merge conflicts
    Signed-off-by: Ahalya Radhakrishnan <ahalya.radhakrishnan@ni.com>

--- acceptance-tests/fake-api/server.js
+++ acceptance-tests/fake-api/server.js
@@ -40,15 +40,15 @@ server.post('/niapp/v1/webapps/query', notebookRoutes.listNotebooks);
 server.get('/ninbparser/v1/notebook/:id', notebookRoutes.getNotebookMetadata);
 
 server.get('/niauth/v1/auth', authRoutes.getUserWorkspaces);
+server.post('/niworkitem/v1/query-workitems', workItemsRoutes.queryWorkItems);
+server.post('/niuser/v1/users/query', usersRoutes.queryUsers);
+server.post('/nitestmonitor/v2/query-products', productsRoutes.queryProducts);
+
 server.get('/nitag/v2/tags-count', tagRoutes.getTagsCount);
 server.post('/nitag/v2/fetch-tags-with-values', tagRoutes.fetchTagsWithValues);
 server.post('/nitaghistorian/v2/tags/query-decimated-history', tagRoutes.queryDecimatedHistory);
 
 server.get('/niuser/v1/workspaccces', authRoutes.getUserWorkspaces);
-
-server.post('/niworkitem/v1/query-workitems', workItemsRoutes.queryWorkItems);
-server.post('/niuser/v1/users/query', usersRoutes.queryUsers);
-server.post('/nitestmonitor/v2/query-products', productsRoutes.queryProducts);
 
 server.use(router);
 server.listen(port);
